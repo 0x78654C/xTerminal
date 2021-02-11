9cdcbf15d0563df8440cd9a3d56fbcbc3890adea
0x78654C <l2xguard@live.com>
2021-02-12 00:56:26 +0200

    Created
    Class for declare current location file path necessary in most of project

---
+++ Core/FileSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class FileSystem
+    {
+        public static string CurrentLocation = @".\Data\curDir.ini"; //Current Location fie path
+
+        
+
+    }
+}
