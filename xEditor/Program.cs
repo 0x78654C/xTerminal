30a5b6024c93dc37e8e17387ae727bfe8870c19c
0x78654C <l2xguard@live.com>
2021-02-14 23:54:58 +0200

    update
    posability to start with new editor

--- xEditor/Program.cs
+++ xEditor/Program.cs
@@ -16,27 +16,92 @@ namespace xEditor
         static void Main(string[] args)
         {
             string file=string.Empty;
+            string set = string.Empty;
             string dlocation = File.ReadAllText(FileSystem.CurrentLocation);
-
+            
+            if (!File.Exists(FileSystem.EditorPath))
+            {
+                File.WriteAllText(FileSystem.EditorPath, "notepad");
+            }
+            string cEditor = File.ReadAllText(FileSystem.EditorPath);
             try
             {
                 file = args[0];
-                var process = new Process();
-                process.StartInfo = new ProcessStartInfo("notepad")
+                set = args[1];
+                if (!file.Contains("set"))
                 {
-                    UseShellExecute = false,
-                    Arguments = dlocation + @"\" + file
 
-                };
+                    if (File.Exists(cEditor))
+                    {
+                        var process = new Process();
+                        process.StartInfo = new ProcessStartInfo(cEditor)
+                        {
+                            UseShellExecute = false,
+                            Arguments = dlocation + @"\" + file
 
-                process.Start();
-                process.WaitForExit();
+                        };
 
+                        process.Start();
+                        process.WaitForExit();
+                    }
+                    else
+                    {
+                    
+                        var process = new Process();
+                        process.StartInfo = new ProcessStartInfo("notepad")
+                        {
+                            UseShellExecute = false,
+                            Arguments = dlocation + @"\" + file
+
+                        };
+
+                        process.Start();
+                        process.WaitForExit();
+                    }
+                }
+                else
+                {
+                    File.WriteAllText(FileSystem.EditorPath, @set);
+                    Console.WriteLine("Your New editor is: " + @set);
+                }
 
             }
             catch
             {
 
+                if (File.Exists(cEditor))
+                {
+                    if (string.IsNullOrEmpty(file))
+                    {
+                        Console.WriteLine("You must type the file name for edit!");
+                    }
+                    else
+                    {
+                        var process = new Process();
+                        process.StartInfo = new ProcessStartInfo(cEditor)
+                        {
+                            UseShellExecute = false,
+                            Arguments = dlocation + @"\" + file
+
+                        };
+
+                        process.Start();
+                        process.WaitForExit();
+                    }
+                }
+                else
+                {
+                    var process = new Process();
+                    process.StartInfo = new ProcessStartInfo("notepad")
+                    {
+                        UseShellExecute = false,
+                        Arguments = dlocation + @"\" + file
+
+                    };
+
+                    process.Start();
+                    process.WaitForExit();
+                }
             }
 
         }
