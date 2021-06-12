813abed1c929402dc4656d14d8da79da95260dad
0x78654C <l2xguard@live.com>
2021-06-12 21:45:12 +0300

    Added function to color a specific line on console.

--- Core/FileSystem.cs
+++ Core/FileSystem.cs
@@ -10,5 +10,18 @@ namespace Core
     {
         public static string CurrentLocation = @".\Data\curDir.ini"; //Current Location file path
         public static string EditorPath = @".\Data\cEditor.ini"; //Current editor file path
+
+
+        /// <summary>
+        /// Change color of a specific line
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="text"></param>
+        public static void ColorConsoleLine(ConsoleColor color, string text)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
     }
 }
