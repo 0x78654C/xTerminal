55290999cf1b1974a964a94cf52442d77e29c6a6
0x78654C <l2xguard@live.com>
2021-08-27 23:53:03 +0300

    refactor: merged File size checkers and file/directory permission checker in one method

--- Core/FileSystem.cs
+++ Core/FileSystem.cs
@@ -1,51 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections;
 using System.IO;
-using System.Threading.Tasks;
 
 namespace Core
 {
     public class FileSystem
     {
-        public static string CurrentLocation = @".\Data\curDir.ini"; //Current Location file path
-        public static string EditorPath = @".\Data\cEditor.ini"; //Current editor file path
-        private static string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+        public static string currentLocation = @".\Data\curDir.ini"; //Current Location file path
+        public static string editorPath = @".\Data\cEditor.ini"; //Current editor file path
+        private static readonly string[] s_sizes = { "B", "KB", "MB", "GB", "TB" };
 
 
         /// <summary>
         /// Get the size of a file.
         /// </summary>
-        /// <param name="fileName"></param>
-        /// <param name="sizeType">Options: 1 - Bytes |
-        /// 2 - Kb |
-        /// 3 - Mb |
-        /// 4 - Gb |
-        /// 5 - Tb | </param>
-        /// <returns></returns>
-        public static string GetFileSize(string fileName)
+        /// <param name="fileName"> Specify the file path.</param>
+        /// <param name="fixedSize">Type of check</param>
+        /// <returns>string</returns>
+        public static string GetFileSize(string fileName, bool fixedSize)
         {
             double len = new FileInfo(fileName).Length;
-            int order = 0;
-            while(len >= 1024 && order < sizes.Length -1)
+            if (fixedSize)
             {
-                order++;
-                len = len / 1024;
+                string sLen = String.Format("{0:0.##}", len);
+                double fLen = Convert.ToDouble(sLen);
+                for (int i = 0; i < 2; i++)
+                {
+                    fLen /= 1024;
+                }
+                return fLen.ToString();
             }
-            return String.Format("{0:0.##} {1}", len, sizes[order]);
-        }
-
-        public static double GetFixedFileSize(string fileName)
-        {
-            double len = new FileInfo(fileName).Length;
-            string sLen = String.Format("{0:0.##}", len);
-            double fLen = Convert.ToDouble(sLen);
-            for(int i = 0; i < 2; i++)
+            else
             {
-                fLen = fLen / 1024;
+                int order = 0;
+                while (len >= 1024 && order < s_sizes.Length - 1)
+                {
+                    order++;
+                    len /= 1024;
+                }
+                return String.Format("{0:0.##} {1}", len, s_sizes[order]);
             }
-            return fLen;
         }
 
         /// <summary>
@@ -58,6 +52,47 @@ namespace Core
             Console.ForegroundColor = color;
             Console.WriteLine(text);
             Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+
+        /// <summary>
+        /// Check write permission to a directory or file.
+        /// </summary>
+        /// <param name="path">Path to the directory or fie you want to check.</param>
+        /// <param name="checkType">File or Directory</param>
+        /// <returns>bool</returns>
+        public static bool CheckPermission(string path, CheckType checkType)
+        {
+            switch (checkType)
+            {
+                case CheckType.Directory:
+                    var dirInfo = new DirectoryInfo(path).GetAccessControl();
+                    if (dirInfo.AreAccessRulesProtected)
+                    {
+                        Console.WriteLine($"Access to the path: {path} is denied!");
+                        return false;
+                    }
+                    return true;
+               
+                case CheckType.File:
+                    var fileInfo = new FileInfo(path).GetAccessControl();
+                    if (fileInfo.AreAccessRulesProtected)
+                    {
+                        Console.WriteLine($"Access to the file: {path} is denied!");
+                        return false;
+                    }
+                    return true;
+                
+                default:
+                    return false;
+            }
+        }
+
+        // Check permission types
+        public enum CheckType
+        {
+            Directory,
+            File
         }
     }
 }
