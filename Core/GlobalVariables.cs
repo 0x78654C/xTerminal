114633aa39d6489ae98569e0c2dfd64e33d27ae0
0x78654C <l2xguard@live.com>
2021-08-30 23:02:44 +0300

    class for storing necesary global variables

---
+++ Core/GlobalVariables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    /// <summary>
+    /// Declaration of necessary global variables
+    /// </summary>
+    public class GlobalVariables
+    {
+        public static string currentLocation = @".\Data\curDir.ini";                 // Current Location file path
+        public static string editorPath = @".\Data\cEditor.ini";                     // Current editor file path
+    }
+}
