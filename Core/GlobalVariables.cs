572dc5e62df414db0cfb444742eaefcbd46f1979
0x78654C <l2xguard@live.com>
2021-08-31 00:15:30 +0300

    added global variables

--- Core/GlobalVariables.cs
+++ Core/GlobalVariables.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core
 {
@@ -11,7 +7,8 @@ namespace Core
     /// </summary>
     public class GlobalVariables
     {
-        public static string currentLocation = @".\Data\curDir.ini";                 // Current Location file path
-        public static string editorPath = @".\Data\cEditor.ini";                     // Current editor file path
+        public static string currentLocation = @".\Data\curDir.ini";                              // Current Location file path
+        public static string editorPath = @".\Data\cEditor.ini";                                  // Current editor file path
+        public static string rootPath= "C:\\";  // Get current users profile folder path.
     }
 }
