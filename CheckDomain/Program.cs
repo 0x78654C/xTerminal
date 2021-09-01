c4ba189c2e1152d0ad55e61ff16a5e4da4b4beb6
0x78654C <l2xguard@live.com>
2021-09-01 23:04:51 +0300

    Added project for checking a domain name or ip address is online or down.

---
+++ CheckDomain/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ping = Core.NetWork;
+
+namespace CheckDomain
+{
+
+    /// <summary>
+    /// Check if an Domain/IP is up.
+    /// </summary>
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string input = args[0];
+                if (ping.PingHost(input))
+                {
+                    Console.WriteLine($"{input} is online!");
+                    return;
+                }
+                Console.WriteLine($"{input} is down!");
+            }
+            catch
+            {
+                Console.WriteLine("You must specify a domain or an IP address to check . Ex.: icheck google.com");
+            }
+        }
+    }
+}
