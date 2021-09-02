67a4e9903530ccc4ea5e304f62fc1696b193030b
0x78654C <l2xguard@live.com>
2021-09-02 22:14:55 +0300

    WMI info grabber by a specified query and scope. Thank you @mkbmain for this

---
+++ Core/Hardware/WMIDetails.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace Core.Hardware
+{
+    public static class WMIDetails
+    {
+        /// <summary>
+        /// Outputs WMI class details 
+        /// </summary>
+        /// <param name="query">WMI querry</param>
+        /// <param name="scope">Scope definition</param>
+        /// <returns>string</returns>
+        public static string GetWMIDetails(string query, string scope = null)
+        {
+            ManagementObjectSearcher moSearcher = scope == null ? new ManagementObjectSearcher(query) : new ManagementObjectSearcher(scope, query);
+            StringBuilder sb = new StringBuilder();
+            foreach (ManagementObject wmi_HD in moSearcher.Get())
+            {
+                var properties = wmi_HD.Properties;
+
+                foreach (var item in properties)
+                {
+                    if (wmi_HD[item.Name] == null || string.IsNullOrWhiteSpace(wmi_HD[item.Name].ToString()))
+                    {
+                        continue;
+                    }
+
+                    sb.AppendLine($"{item.Name}: {FormatOutputValue(wmi_HD[item.Name]) }");
+
+                }
+
+                sb.AppendLine(Environment.NewLine + string.Join("", Enumerable.Range(1, 30).Select(t => '-')) + Environment.NewLine);
+            }
+            return sb.ToString();
+        }
+
+        private static string FormatOutputValue(object item)
+        {
+            var value = item.ToString();
+            if (item is System.Collections.IEnumerable && !(item is string))
+            {
+                value = "";
+                foreach (var item2 in (System.Collections.IEnumerable)item)
+                {
+                    value += item2 + " ";
+                }
+            }
+            return value;
+        }
+    }
+}
