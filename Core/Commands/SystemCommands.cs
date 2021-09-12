9fe105de5c5a17360fe3f617811f68d53352f61b
0x78654C <l2xguard@live.com>
2021-09-12 23:02:01 +0300

    added class for system commands

---
+++ Core/Commands/SystemCommands.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Commands
+{
+   public static class SystemCommands
+    {
+        /// <summary>
+        /// Shutdown command using command promt.
+        /// </summary>
+        public static void ShutDownCmd()
+        {
+            var process = new Process();
+            process.StartInfo = new ProcessStartInfo("cmd.exe")
+            {
+                UseShellExecute = false,
+                Arguments = "/c shutdown /s /f /t 1"
+
+            };
+            process.Start();
+            process.WaitForExit();
+        }
+
+        /// <summary>
+        /// Reboot command using command promt.
+        /// </summary>
+        public static void RebootCmd()
+        {
+            var process = new Process();
+            process.StartInfo = new ProcessStartInfo("cmd.exe")
+            {
+                UseShellExecute = false,
+                Arguments = "/c shutdown /r /f /t 1"
+
+            };
+            process.Start();
+            process.WaitForExit();
+        }
+
+        /// <summary>
+        /// Logoff current loged user using command promt.
+        /// </summary>
+        public static void LogoffCmd()
+        {
+            var process = new Process();
+            process.StartInfo = new ProcessStartInfo("cmd.exe")
+            {
+                UseShellExecute = false,
+                Arguments = "/c shutdown /l /f"
+
+            };
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
