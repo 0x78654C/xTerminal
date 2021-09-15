fe873d50955478a349930e53350ffc939747f044
0x78654C <l2xguard@live.com>
2021-09-15 23:38:42 +0300

    - added new command ls -c for count total files in current and subdirectories and directories/subdirectories count - added count of filles and directories in current location

--- ListDirectories/Program.cs
+++ ListDirectories/Program.cs
@@ -7,13 +7,25 @@ namespace ListDirectories
     /*List files and directories*/
     class Program
     {
+        private static string s_currentDirectory = string.Empty;
         static void Main(string[] args)
         {
             try
             {
+                s_currentDirectory = File.ReadAllText(GlobalVariables.currentLocation);
+
                 if (args[0] == "-s")
                 {
                     ListDirFile(true);
+                    return;
+                }
+                else if (args[0] == "-c")
+                {
+                    int files = Directory.GetFiles(s_currentDirectory,"*.*", SearchOption.AllDirectories).Length;
+                    var directories = Directory.GetDirectories(s_currentDirectory,"*",SearchOption.AllDirectories).Length;
+                    Console.WriteLine($"Conting total directories/subdirectories and files on current location....\n");
+                    Console.WriteLine($"Total directories/subdirectories: {directories}");
+                    Console.WriteLine($"Total files (include subdirectories): {files}");
                     return;
                 }
                 ListDirFile(false);
@@ -24,13 +36,14 @@ namespace ListDirectories
             }
         }
 
+
         private static void ListDirFile(bool sizeCheck)
         {
-            string cDir = File.ReadAllText(GlobalVariables.currentLocation);
-            if (Directory.Exists(cDir))
+
+            if (Directory.Exists(s_currentDirectory))
             {
-                var files = Directory.GetFiles(cDir);
-                var directories = Directory.GetDirectories(cDir);
+                var files = Directory.GetFiles(s_currentDirectory);
+                var directories = Directory.GetDirectories(s_currentDirectory);
                 DirectoryInfo directoryInfo;
                 FileInfo fileInfo;
                 foreach (var dir in directories)
@@ -49,17 +62,21 @@ namespace ListDirectories
                     {
                         Console.WriteLine(fileInfo.Name);
                     }
+
                 }
                 if (sizeCheck)
                 {
-                    string currentDirectorySize = FileSystem.GetDirSize(new DirectoryInfo(cDir));
+                    string currentDirectorySize = FileSystem.GetDirSize(new DirectoryInfo(s_currentDirectory));
                     Console.WriteLine("---------------------------------------------\n");
                     Console.WriteLine($"Current directory size: {currentDirectorySize}\n");
                 }
+                Console.WriteLine("---------------------------------------------\n");
+                Console.WriteLine($"Total directories: {directories.Length}");
+                Console.WriteLine($"Total files: {files.Length}");
             }
             else
             {
-                FileSystem.ErrorWriteLine($"Directory '{cDir}' dose not exist!");
+                FileSystem.ErrorWriteLine($"Directory '{s_currentDirectory}' dose not exist!");
             }
         }
     }
