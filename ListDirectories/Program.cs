2cbedeef94140a45250139e30d055b3585b9d895
0x78654C <l2xguard@live.com>
2021-09-20 00:53:47 +0300

    added -hl for hightlight text in list

--- ListDirectories/Program.cs
+++ ListDirectories/Program.cs
@@ -12,32 +12,45 @@ namespace ListDirectories
         {
             try
             {
-                s_currentDirectory = RegistryManagement.regKey_Read(GlobalVariables.regKeyName, GlobalVariables.regCurrentDirectory);;
+                s_currentDirectory = RegistryManagement.regKey_Read(GlobalVariables.regKeyName, GlobalVariables.regCurrentDirectory); ;
 
                 if (args[0] == "-s")
                 {
-                    ListDirFile(true);
+                    ListDirFile(true,false,"");
                     return;
                 }
                 else if (args[0] == "-c")
                 {
-                    int files = Directory.GetFiles(s_currentDirectory,"*.*", SearchOption.AllDirectories).Length;
-                    var directories = Directory.GetDirectories(s_currentDirectory,"*",SearchOption.AllDirectories).Length;
+                    int files = Directory.GetFiles(s_currentDirectory, "*.*", SearchOption.AllDirectories).Length;
+                    var directories = Directory.GetDirectories(s_currentDirectory, "*", SearchOption.AllDirectories).Length;
                     Console.WriteLine($"Conting total directories/subdirectories and files on current location....\n");
                     Console.WriteLine($"Total directories/subdirectories: {directories}");
                     Console.WriteLine($"Total files (include subdirectories): {files}");
                     return;
+                }else if (args[0] == "-hl")
+                {
+                    try
+                    {
+                        string higlightedText = args[1];
+                        ListDirFile(false, true, higlightedText);
+                    }
+                    catch 
+                    {
+                        FileSystem.ErrorWriteLine("Check command. You must provide a text to highlight!");
+                    }
+                    return;
                 }
-                ListDirFile(false);
+
+                ListDirFile(false,false,"");
             }
             catch
             {
-                ListDirFile(false);
+                ListDirFile(false, false, "");
             }
         }
 
 
-        private static void ListDirFile(bool sizeCheck)
+        private static void ListDirFile(bool sizeCheck, bool highlight, string highLightText)
         {
 
             if (Directory.Exists(s_currentDirectory))
@@ -49,20 +62,64 @@ namespace ListDirectories
                 foreach (var dir in directories)
                 {
                     directoryInfo = new DirectoryInfo(dir);
-                    FileSystem.ColorConsoleTextLine(ConsoleColor.DarkCyan, directoryInfo.Name);
+                    if (highlight && highLightText.Length >0)
+                    {
+                        if (directoryInfo.Name.ToLower().Contains(highLightText.ToLower()))
+                        {
+                            FileSystem.ColorConsoleTextLine(ConsoleColor.Red, directoryInfo.Name);
+                        }
+                        else
+                        {
+                            FileSystem.ColorConsoleTextLine(ConsoleColor.DarkCyan, directoryInfo.Name);
+                        }
+                    }
+                    else
+                    {
+                        FileSystem.ColorConsoleTextLine(ConsoleColor.DarkCyan, directoryInfo.Name);
+
+                    }
                 }
                 foreach (var file in files)
                 {
                     fileInfo = new FileInfo(file);
                     if (sizeCheck)
                     {
-                        Console.WriteLine(fileInfo.Name.PadRight(50, ' ') + $"Size: {FileSystem.GetFileSize(file, false)}");
+                        if (highlight && highLightText.Length>0)
+                        {
+                            if (fileInfo.Name.ToLower().Contains(highLightText.ToLower()))
+                            {
+                                FileSystem.ColorConsoleTextLine(ConsoleColor.Yellow, fileInfo.Name.PadRight(50, ' ') + $"Size: {FileSystem.GetFileSize(file, false)}");
+                            }
+                            else
+                            {
+                                Console.WriteLine(fileInfo.Name.PadRight(50, ' ') + $"Size: {FileSystem.GetFileSize(file, false)}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine(fileInfo.Name.PadRight(50, ' ') + $"Size: {FileSystem.GetFileSize(file, false)}");
+
+                        }
                     }
                     else
                     {
-                        Console.WriteLine(fileInfo.Name);
-                    }
+                        if (highlight && highLightText.Length>0)
+                        {
 
+                            if (fileInfo.Name.ToLower().Contains(highLightText.ToLower()))
+                            {
+                                FileSystem.ColorConsoleTextLine(ConsoleColor.Yellow, fileInfo.Name);
+                            }
+                            else
+                            {
+                                Console.WriteLine(fileInfo.Name);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine(fileInfo.Name);
+                        }
+                    }
                 }
                 if (sizeCheck)
                 {
