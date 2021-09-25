a5bf5334749ff137f7e7e31df3c6b5d1eb672ff1
0x78654C <l2xguard@live.com>
2021-09-25 21:30:25 +0300

    categorized commands

--- Shell/Shell.cs
+++ Shell/Shell.cs
@@ -243,41 +243,49 @@ namespace Shell
 xTerminal v1.0 Copyright @ 2020-2021 0x078654c
 This is the full list of commands that can be used in xTerminal:
 
+     
+----------------------- System -----------------------
     ls -- List directories and files on a directory. (ls -s for size display, ls -c for count files and directories (subdirs too)
           ls -hl text  : highlight specific files/directories with that text)
     hcmd -- Displays a list of previous commands typed in terminal. Ex.: hcmd 10 -> displays last 10 commands used. 
+    chistory -- Clears the current history of commands!
+    start -- Starts an application. Ex.: start C:\Windows\System32\notepad.exe
     clear --  Cleares the console.
     cd -- Sets the currnet directory. (cd .. for parent directory)
-    ispeed -- Checks the internet speed with Google.
-    icheck -- Checks if a Domain or IP address is online.
-    md5 -- Checks the md5 checksum of a file.
-    extip -- Displays the current external IP address.
-    ifconfig -- Display onboard Network Interface Cards configguration (Ethernet and Wireless)
-    wget -- Download files from a specific website.
-    fcopy -- Copies a file with CRC checksum control.
-    frename -- Renames a file in a specific directory(s).
-    fmove -- Moves a file with CRC checksum control.
-    cmd --  Opens Windows Command Prompt.
     ps -- Opens Windows Powershell.
-    cat -- Displays the content of a file. Can use following parameters:
-           -s   : Output lines containing a provided text from a file.
-           -so  : Saves the lines containing a provided text from a file.
-           -sm  : Output lines containing a provided text from multiple fies.
-           -smo : Saves the lines containing a provided text from mutiple files in current path location.
-    del -- Deletes a file or folder without recover.
+    cmd --  Opens Windows Command Prompt.
     reboot -- It force reboots the Windows OS.
     shutdown --  It force shutdown the Windows OS.
     logoff -- It force logoff current user.
     bios -- Displays BIOS information on local machine or remote.
     sinfo -- Displays Storage devices information on local machine or remote.
+
+---------------------- File System ---------------------
+    cat -- Displays the content of a file. Can use following parameters:
+           -s   : Output lines containing a provided text from a file.
+           -so  : Saves the lines containing a provided text from a file.
+           -sm  : Output lines containing a provided text from multiple fies.
+           -smo : Saves the lines containing a provided text from multiple files in current path location.
     mkdir -- It creates a directory in the current place.
     mkfile -- It creates a file in the current place.
+    fcopy -- Copies a file with CRC checksum control.
+    frename -- Renames a file in a specific directory(s).
+    fmove -- Moves a file with CRC checksum control.
     edit -- Opens a file in Notepad(default). To set a new text editor you must use following command: edit set ""Path to editor""
+    del -- Deletes a file or folder without recover.
     cp -- Check file/folder permissions.
+    md5 -- Checks the md5 checksum of a file.
+
+---------------------- Networking ----------------------
+    ifconfig -- Display onboard Network Interface Cards configuration (Ethernet and Wireless)
+    ispeed -- Checks the internet speed with Google.
+    icheck -- Checks if a Domain or IP address is online.
+    extip -- Displays the current external IP address.
+    wget -- Download files from a specific website.
     speedtest -- Makes an internet speed test based on speedtest.net API.
     email -- Email sender client for Microsoft (all), Yahoo, Gmail!
-    chistory -- Clears the current history of commands!
-    start -- Starts an application. Ex.: start C:\Windows\System32\notepad.exe
+    
+------------------------ Games -------------------------
     flappy -- Play Flappy Birds in console!
  
 
