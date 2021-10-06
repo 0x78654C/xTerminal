be590db4e35b445c19cb0f955d1d3f319783fe42
0x78654C <l2xguard@live.com>
2021-10-06 23:00:35 +0300

    added new global variables and removed unused one

--- Core/GlobalVariables.cs
+++ Core/GlobalVariables.cs
@@ -7,12 +7,14 @@ namespace Core
     /// </summary>
     public class GlobalVariables
     {
-        public static string currentLocation = Environment.CurrentDirectory + @"\Data\curDir.ini";                              // Current Location file path
-        public static string editorPath = Environment.CurrentDirectory + @"\Data\cEditor.ini";                                  // Current editor file path
         public static string regKeyName = "xTerminal";
         public static string regCurrentDirectory = "CurrentDirectory";
         public static string regCurrentEitor = "CurrentEditor";
         public static string rootPath = "C:\\";  // Get current users profile folder path.
         public static string terminalTitle = "xTerminal v1.0";  // Terminal title
+        public static readonly string accountName = Environment.UserName;
+        public static readonly string computerName = Environment.MachineName; //extract machine name
+        public static string historyFilePath = $"C:\\Users\\{accountName}\\AppData\\Local\\xTerminal";
+        public static string historyFile = historyFilePath + "\\History.db";
     }
 }
