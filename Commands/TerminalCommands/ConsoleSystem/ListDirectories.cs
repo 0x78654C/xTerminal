70fa0fa8277dca5831a566b93a3f3ed35238f995
0x78654C <l2xguard@live.com>
2021-10-07 23:46:22 +0300

    added ls -o command  for saveing ouput to a file changed  DisplaySubDirectoryAndFileCounts function to use recursion

--- Commands/TerminalCommands/ConsoleSystem/ListDirectories.cs
+++ Commands/TerminalCommands/ConsoleSystem/ListDirectories.cs
@@ -1,5 +1,6 @@
 ﻿using Core;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -8,6 +9,11 @@ namespace Commands.TerminalCommands.ConsoleSystem
     public class ListDirectories : ITerminalCommand
     {
         private static string s_currentDirectory = string.Empty;
+
+        private static int s_countFiles = 0;
+        private static int s_countDirectories = 0;
+        private static List<string> s_listFiles = new List<string>();
+        private static List<string> s_listDirs = new List<string>();
 
         public string Name => "ls";
 
@@ -31,12 +37,25 @@ namespace Commands.TerminalCommands.ConsoleSystem
                 s_currentDirectory =
                     RegistryManagement.regKey_Read(GlobalVariables.regKeyName, GlobalVariables.regCurrentDirectory);
 
-                // Display directory and file information
-                DisplayCurrentDirectoryFiles(arg.ContainsParameter("-s"), highlightSearchText);
+                // Save ls output to a file
+                if (arg.ContainsParameter("-o"))
+                {
+                    SaveLSOutput(arg.ParameterAfter("-o"));
+                }
+                else
+                {
+                    // Display directory and file information
+                    DisplayCurrentDirectoryFiles(arg.ContainsParameter("-s"), highlightSearchText, false);
+                }
 
                 if (arg.ContainsParameter("-c"))
                 {
-                    DisplaySubDirectoryAndFileCounts();
+                    Console.WriteLine($"\nCounting total directories/subdirectories and files on current location....\n");
+                    DisplaySubDirectoryAndFileCounts(s_currentDirectory);
+                    Console.WriteLine($"Total directories/subdirectories: {s_countDirectories}");
+                    Console.WriteLine($"Total files (include subdirectories): {s_countFiles}");
+                    s_countDirectories = 0;
+                    s_countFiles = 0;
                 }
             }
             catch (IndexOutOfRangeException)
@@ -54,17 +73,35 @@ namespace Commands.TerminalCommands.ConsoleSystem
             }
         }
 
-        private static void DisplaySubDirectoryAndFileCounts()
+        private static void SaveLSOutput(string path)
         {
-            var files = Directory.GetFiles(s_currentDirectory, "*.*", SearchOption.AllDirectories).Count();
-            var directories = Directory.GetDirectories(s_currentDirectory, "*", SearchOption.AllDirectories).Count();
-
-            Console.WriteLine($"Counting total directories/subdirectories and files on current location....\n");
-            Console.WriteLine($"Total directories/subdirectories: {directories}");
-            Console.WriteLine($"Total files (include subdirectories): {files}");
+            DisplayCurrentDirectoryFiles(false, "", true);
+            string dirList = "Directories:\n";
+            dirList += string.Join("\n", s_listDirs);
+            string fileList = "\n\nFiles:\n";
+            fileList += string.Join("\n", s_listFiles);
+            string finalList = dirList + fileList;
+            Console.WriteLine(FileSystem.SaveFileOutput(path, s_currentDirectory, finalList));
         }
 
-        private static void DisplayCurrentDirectoryFiles(bool displaySizes, string highlightSearchText)
+        private static void DisplaySubDirectoryAndFileCounts(string currentDirectory)
+        {
+            var files = Directory.GetFiles(currentDirectory);
+            var directories = Directory.GetDirectories(currentDirectory);
+
+            foreach (var file in files)
+            {
+                s_countFiles++;
+            }
+
+            foreach (var dir in directories)
+            {
+                DisplaySubDirectoryAndFileCounts(dir);
+                s_countDirectories++;
+            }
+        }
+
+        private static void DisplayCurrentDirectoryFiles(bool displaySizes, string highlightSearchText, bool saveToFile)
         {
             if (!Directory.Exists(s_currentDirectory))
             {
@@ -72,9 +109,16 @@ namespace Commands.TerminalCommands.ConsoleSystem
                 return;
             }
 
-            DisplaySubDirectories(highlightSearchText);
-
-            DisplayFiles(highlightSearchText, displaySizes);
+            if (saveToFile)
+            {
+                DisplaySubDirectories(highlightSearchText, saveToFile);
+                DisplayFiles(highlightSearchText, displaySizes, saveToFile);
+            }
+            else
+            {
+                DisplaySubDirectories(highlightSearchText, saveToFile);
+                DisplayFiles(highlightSearchText, displaySizes, saveToFile);
+            }
 
             if (displaySizes)
             {
@@ -90,7 +134,7 @@ namespace Commands.TerminalCommands.ConsoleSystem
             Console.WriteLine($"Total files: {Directory.GetFiles(s_currentDirectory).Length}");
         }
 
-        private static void DisplaySubDirectories(string highlightSearchText)
+        private static void DisplaySubDirectories(string highlightSearchText, bool saveToFile)
         {
             foreach (var dir in Directory.GetDirectories(s_currentDirectory))
             {
@@ -103,12 +147,19 @@ namespace Commands.TerminalCommands.ConsoleSystem
                 }
                 else
                 {
-                    FileSystem.ColorConsoleTextLine(ConsoleColor.DarkCyan, directoryInfo.Name);
+                    if (saveToFile)
+                    {
+                        s_listDirs.Add(directoryInfo.Name);
+                    }
+                    else
+                    {
+                        FileSystem.ColorConsoleTextLine(ConsoleColor.DarkCyan, directoryInfo.Name);
+                    }
                 }
             }
         }
 
-        private static void DisplayFiles(string highlightSearchText, bool displaySizes)
+        private static void DisplayFiles(string highlightSearchText, bool displaySizes, bool saveToFile)
         {
             // This LINQ statement converts a list of string file names to FileInfo objects
             var files = Directory.GetFiles(s_currentDirectory).Select(f => new FileInfo(f));
@@ -116,8 +167,14 @@ namespace Commands.TerminalCommands.ConsoleSystem
             foreach (var file in files)
             {
                 string formattedText = GetFormattedFileInfoText(file, displaySizes);
-
-                DisplayFileInfoText(formattedText, highlightSearchText);
+                if (saveToFile)
+                {
+                    s_listFiles.Add(file.Name);
+                }
+                else
+                {
+                    DisplayFileInfoText(formattedText, highlightSearchText);
+                }
             }
         }
 
