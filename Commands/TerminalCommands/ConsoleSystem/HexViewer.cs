e4c0856d61e37c2f58ae18f51cc2b014f51ca7f7
0x78654C <l2xguard@live.com>
2021-10-08 13:22:59 +0300

    refactoring: removeing unused declaration

--- Commands/TerminalCommands/ConsoleSystem/HexViewer.cs
+++ Commands/TerminalCommands/ConsoleSystem/HexViewer.cs
@@ -1,11 +1,5 @@
 ﻿using Core;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms.Design;
-using System.ComponentModel.Design;
 using System.IO;
 
 namespace Commands.TerminalCommands.ConsoleSystem
