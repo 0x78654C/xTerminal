22d9faccb254d689c10f2659c0965d9db8cfc8af
0x78654C <l2xguard@live.com>
2021-10-08 13:25:33 +0300

    added new ping command class with -r param for number to replies to make

---
+++ Commands/TerminalCommands/Network/Ping.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Core;
+
+namespace Commands.TerminalCommands.Network
+{
+    public class Ping : ITerminalCommand
+    {
+        public string Name => "ping";
+
+        public void Execute(string args)
+        {
+            try
+            {
+                string[] arg = args.Split(' ');
+                if (args.ContainsText("-r"))
+                {
+                    int pingReplays= Int32.Parse(arg.ParameterAfter("-r"));
+                    NetWork.PingMain(arg.ParameterAfter("ping"),pingReplays);
+                    return;
+                }
+                NetWork.PingMain(arg.ParameterAfter("ping"), 4);
+
+            }
+            catch (Exception e)
+            {
+                FileSystem.ErrorWriteLine(e.Message);
+            }
+        }
+    }
+}
