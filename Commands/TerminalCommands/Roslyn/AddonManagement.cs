36895b4d11f1c2f37e25ba1e5709712c45e87d2f
0x78654C <l2xguard@live.com>
2021-10-13 21:55:39 +0300

    corection on paramter

--- Commands/TerminalCommands/Roslyn/AddonManagement.cs
+++ Commands/TerminalCommands/Roslyn/AddonManagement.cs
@@ -36,7 +36,7 @@ namespace Commands.TerminalCommands.Roslyn
                 Example: ! <command_name> -p <parameters>
    -add   :  Adds new code from a file and stores in Add-ons directory under xTerminal.exe
              current directory with a command name.
-                Example: ! -add <file_name_with_code> -d <command_name>|<command_description>
+                Example: ! -add <file_name_with_code> -c <command_name>|<command_description>
    -del   :  Deletes an Add-on.
                 Example: ! -del <command_name>
    -list  :  Display the list of the saved add-ons with description.
