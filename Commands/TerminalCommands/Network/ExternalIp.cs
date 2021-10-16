49f9f09bbfb388a5cd5a7dd67418f2c29582a9f8
0x78654C <l2xguard@live.com>
2021-10-16 16:45:08 +0300

    added class info and some name refactor

--- Commands/TerminalCommands/Network/ExternalIp.cs
+++ Commands/TerminalCommands/Network/ExternalIp.cs
@@ -7,17 +7,20 @@ namespace Commands.TerminalCommands.Network
 {
     public class ExternalIp : ITerminalCommand
     {
+        /*
+         * Display machine external IP.
+         */
         public string Name => "extip";
 
         public void Execute(string arg)
         {
             try
             {
-                string blackshit;
-                blackshit = (new WebClient()).DownloadString("http://checkip.dyndns.org");
-                blackshit = (new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"))
-                             .Matches(blackshit)[0].ToString();
-                Console.WriteLine("Your external IP address is: " + blackshit);
+                string externalIP;
+                externalIP = (new WebClient()).DownloadString("http://checkip.dyndns.org");
+                externalIP = (new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"))
+                             .Matches(externalIP)[0].ToString();
+                Console.WriteLine("Your external IP address is: " + externalIP);
             }
             catch { FileSystem.ErrorWriteLine("Canno't verify external IP. Check your internet connection!"); }
         }
