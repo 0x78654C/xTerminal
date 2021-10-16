ba70d565578bdee49e775a75c2f05d657e610eaa
0x78654C <l2xguard@live.com>
2021-10-16 16:46:43 +0300

    added class information

--- Commands/TerminalCommands/Network/NetworkInterfaceCheck.cs
+++ Commands/TerminalCommands/Network/NetworkInterfaceCheck.cs
@@ -5,6 +5,9 @@ namespace Commands.TerminalCommands.Network
 {
     public class NetworkInterfaceCheck : ITerminalCommand
     {
+        /*
+         Display NIC's configuration. 
+         */
         public string Name => "ifconfig";
 
         public void Execute(string args)
