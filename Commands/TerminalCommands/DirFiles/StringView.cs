8fb1d0e9e49b4e9014fe8cf7ae354f8a6d65e566
0x78654C <l2xguard@live.com>
2021-10-22 00:14:13 +0300

    fixed ouput the error to normal message

--- Commands/TerminalCommands/DirFiles/StringView.cs
+++ Commands/TerminalCommands/DirFiles/StringView.cs
@@ -165,7 +165,7 @@ namespace Commands.TerminalCommands.DirFiles
             }
             catch (Exception e)
             {
-                FileSystem.ErrorWriteLine(e.ToString());
+                FileSystem.ErrorWriteLine(e.Message);
             }
         }
     }
