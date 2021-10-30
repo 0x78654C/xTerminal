ddfeb1e7b9d3d2baedbd7292097d307e24adc99f
0x78654C <l2xguard@live.com>
2021-10-30 21:32:09 +0300

    refactored variables names and added global variables for password manager

--- Core/GlobalVariables.cs
+++ Core/GlobalVariables.cs
@@ -12,17 +12,17 @@ namespace Core
     {
         private static string process = Process.GetCurrentProcess().Id.ToString();
         public static string regKeyName = "xTerminal";
-        public static string regCurrentDirectory = "CurrentDirectory";
         public static string regCurrentEitor = "CurrentEditor";
         public static string regUI = "UI";
         public static string rootPath = Path.GetPathRoot(Environment.SystemDirectory);
         public static string terminalTitle = "xTerminal v1.0.1"; 
         public static readonly string accountName = Environment.UserName;
         public static readonly string computerName = Environment.MachineName; 
-        public static string historyFilePath = $"{rootPath}Users\\{accountName}\\AppData\\Local\\xTerminal";
-        public static string currentDirectory = historyFilePath + $"\\{process}cDir.t";
-        public static string uiSettings = historyFilePath + $"\\{process}ui.t";
-        public static string historyFile = historyFilePath + "\\History.db";
+        public static string terminalWorkDirectory = $"{rootPath}Users\\{accountName}\\AppData\\Local\\xTerminal";
+        public static string passwordManagerDirectory = $"{terminalWorkDirectory}\\Pwm\\";
+        public static string currentDirectory = terminalWorkDirectory + $"\\{process}cDir.t";
+        public static string uiSettings = terminalWorkDirectory + $"\\{process}ui.t";
+        public static string historyFile = terminalWorkDirectory + "\\History.db";
         public static string addonDirectory = Directory.GetCurrentDirectory() + "\\Add-ons";
         public static List<string> excludeDirectories = new List<string>() { "System Volume Information", "$Recycle.Bin" };
         public static List<string> excludeFiles = new List<string>() { "pagefile.sys"};
