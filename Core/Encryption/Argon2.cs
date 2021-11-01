1f29c2f3a4a2927202ff86df29833a26101675e2
0x78654C <l2xguard@live.com>
2021-11-01 22:39:07 +0200

    refactor

--- Core/Encryption/Argon2.cs
+++ Core/Encryption/Argon2.cs
@@ -16,12 +16,14 @@ namespace Core.Encryption
 		/// <returns></returns>
 		public static byte[] Argon2HashPassword(string password)
 		{
-			s_argon2 = new Argon2id(Encoding.UTF8.GetBytes(password));
-			s_argon2.Salt = Encoding.UTF8.GetBytes(password.Substring(2, 10));
-			s_argon2.DegreeOfParallelism = 2;
-			s_argon2.Iterations = 40;
-			s_argon2.MemorySize = 4096;
-			return s_argon2.GetBytes(32);
+            s_argon2 = new Argon2id(Encoding.UTF8.GetBytes(password))
+            {
+                Salt = Encoding.UTF8.GetBytes(password.Substring(2, 10)),
+                DegreeOfParallelism = 2,
+                Iterations = 40,
+                MemorySize = 4096
+            };
+            return s_argon2.GetBytes(32);
 		}
 	}
 }
