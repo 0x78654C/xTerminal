eb2455e1c480e2eb89a016d74fed3e803c049946
0x78654C <l2xguard@live.com>
2021-11-06 22:55:21 +0200

    moved hidden password to PasswordValidator.cs and cleanup

--- Core/eMailS.cs
+++ Core/eMailS.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Net.Mail;
-using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace Core
 {
@@ -93,91 +91,6 @@ namespace Core
                 smtpServer = "No valid SMTP Server";
             }
             return smtpServer;
-        }
-
-        /// <summary>
-        /// Hidding password imput for strings
-        /// </summary>
-        /// <returns></returns>
-        public static string GetHiddenConsoleInput()
-        {
-            string pwd = string.Empty;
-            while (true)
-            {
-                ConsoleKeyInfo i = Console.ReadKey(true);
-                if (i.Key == ConsoleKey.Enter)
-                {
-                    break;
-                }
-                else if (i.Key == ConsoleKey.Backspace)
-                {
-                    if (pwd.Length > 0)
-                    {
-                        pwd.Remove(pwd.Length - 1);
-                        Console.Write("\b \b");
-                    }
-                }
-                else if (i.KeyChar != '\u0000')
-                {
-                    pwd += (i.KeyChar);
-                    Console.Write("*");
-                }
-            }
-            return pwd;
-        }
-
-        /// <summary>
-        /// Password complexity check: digit, upper case and lower case.
-        /// </summary>
-        /// <param name="password">Password string.</param>
-        /// <returns>bool</returns>
-        public static bool ValidatePassword(string password)
-        {
-            string patternPassword = @"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,500}$";
-            if (!string.IsNullOrEmpty(password))
-            {
-                if (CheckSpaceChar(password))
-                {
-                    return false;
-                }
-
-                if (!Regex.IsMatch(password, patternPassword))
-                {
-                    return false;
-                }
-
-                if (!SpecialCharCheck(password))
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// Check string for special character.
-        /// </summary>
-        /// <param name="input">Password string.</param>
-        /// <returns></returns>
-        private static bool SpecialCharCheck(string input)
-        {
-            string specialChar = @"\|!#$%&/()=?»«@£§€{}.-;'<>_,";
-            if (input.IndexOfAny(specialChar.ToCharArray()) > -1)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// Check for empty space in password.
-        /// </summary>
-        /// <param name="input">Password string.</param>
-        /// <returns></returns>
-        private static bool CheckSpaceChar(string input)
-        {
-            if (input.Contains(" ")) { return true; }
-            return false;
         }
     }
 }
