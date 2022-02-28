31247274da4986e07ea02b7ce49e9c524992419e
0x78654C <l2xguard@live.com>
2022-02-28 15:05:00 +0200

    added commands: cport stimeout (for setting check port time out) and cport rtimeout to read the current time out.

--- Commands/TerminalCommands/Network/PortScanner.cs
+++ Commands/TerminalCommands/Network/PortScanner.cs
@@ -8,10 +8,13 @@ namespace Commands.TerminalCommands.Network
         /* Connection status check for an port (opened or closed) */
 
         public string Name => "cport";
+        private static int s_timeOut = 0;
         private static string s_helpMessage = @"Usage of cport command:
 
     Example 1: cport IPAddress/HostName -p 80   (checks if port 80 is open)
     Example 2: cport IPAddress/HostName -p 1-200   (checks if any port is open from 1 to 200)
+    Example 3: cport stimeout 100   (Set check port time out in milliseconds. Default value is 500.)
+    Example 4: cport rtimeout   (Reads the current time out value)
 
     Port range scan can be canceled with CTRL+X key combination.    
 ";
@@ -20,6 +23,32 @@ namespace Commands.TerminalCommands.Network
         {
             try
             {
+                string cTimeOut = RegistryManagement.regKey_Read(GlobalVariables.regKeyName, GlobalVariables.regCportTimeOut);
+                if (FileSystem.IsNumberAllowed(cTimeOut) && !string.IsNullOrEmpty(cTimeOut))
+                    s_timeOut = Int32.Parse(cTimeOut);
+                else
+                {
+                    s_timeOut = 500;
+                    RegistryManagement.regKey_WriteSubkey(GlobalVariables.regKeyName, GlobalVariables.regCportTimeOut, "500");
+                }
+
+                if (args.StartsWith($"{Name} stimeout"))
+                {
+                    string timeOut = args.SplitByText("stimeout ", 1);
+                    RegistryManagement.regKey_WriteSubkey(GlobalVariables.regKeyName, GlobalVariables.regCportTimeOut, timeOut);
+                    Console.Write("New check port time out is: ");
+                    FileSystem.ColorConsoleTextLine(ConsoleColor.Green, timeOut+" ms");
+                    return;
+                }
+
+                if (args == $"{Name} rtimeout")
+                {
+                    string timeOut = RegistryManagement.regKey_Read(GlobalVariables.regKeyName, GlobalVariables.regCportTimeOut);
+                    Console.Write("Check port time out is set to: ");
+                    FileSystem.ColorConsoleTextLine(ConsoleColor.Green, timeOut + " ms");
+                    return;
+                }
+
                 if (args.Length == 5)
                 {
                     Console.WriteLine($"Use -h param for {Name} command usage!");
@@ -44,11 +73,11 @@ namespace Commands.TerminalCommands.Network
                     string[] portsRange = portData.Split('-');
                     int minPort = Int32.Parse(portsRange[0].Trim());
                     int maxPort = Int32.Parse(portsRange[1].Trim());
-                    PortScan.RunPortScan(ipAddress, minPort, maxPort);
+                    PortScan.RunPortScan(ipAddress, minPort, maxPort, s_timeOut);
                     return;
                 }
                 int port = Int32.Parse(portData.Trim());
-                PortScan.RunPortScan(ipAddress, port, port);
+                PortScan.RunPortScan(ipAddress, port, port, s_timeOut);
             }
             catch (Exception e)
             {
