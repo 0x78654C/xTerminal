3086cd12adb74a24f6dcb9643f620fe894f00dae
0x78654C <l2xguard@live.com>
2022-03-04 23:49:54 +0200

    added class for check if a assambly is managed or not

---
+++ Core/SystemTools/Roslyn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace Core.SystemTools
+{
+    public  class Roslyn
+    {
+        /// <summary>
+        /// Check if file is managed code lib.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static bool IsManaged(string path)
+        {
+            try
+            {
+                var b = AssemblyName.GetAssemblyName(path);
+                return true;
+            }
+            catch (Exception e)
+            {
+
+            }
+            return false;
+        }
+    }
+}
