6df25902e0f7b3b2a17a592e084104ad288abce0
0x78654C <l2xguard@live.com>
2022-03-20 14:51:41 +0200

    fix access error search stop on cat command

--- Core/Commands/CatCommand.cs
+++ Core/Commands/CatCommand.cs
@@ -72,73 +72,77 @@ namespace Core.Commands
         /// <returns>string</returns>
         public static string MultiFileOutput(string searchString, string currentDir, IEnumerable<string> paths, string savedFile, bool searchAll, string fileName = null)
         {
-            StringBuilder output = new StringBuilder();
-            if (searchAll)
+            try
             {
-                string[] files = Directory.GetFiles(currentDir);
-
-                if (!string.IsNullOrEmpty(fileName))
+                StringBuilder output = new StringBuilder();
+                if (searchAll)
                 {
-                    foreach (var file in files)
-                    {
-                        var fileInfo = new FileInfo(file);
+                    string[] files = Directory.GetFiles(currentDir);
 
-                        if (!string.IsNullOrEmpty(fileName) && fileInfo.Name.Contains(fileName))
+                    if (!string.IsNullOrEmpty(fileName))
+                    {
+                        foreach (var file in files)
+                        {
+                            var fileInfo = new FileInfo(file);
+
+                            if (!string.IsNullOrEmpty(fileName) && fileInfo.Name.Contains(fileName))
+                            {
+                                if (!File.Exists(file))
+                                {
+                                    FileSystem.ErrorWriteLine("File " + file + " dose not exist!");
+                                    continue;
+                                }
+                                output.AppendLine(LineOutput(file, currentDir, searchString));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        foreach (var file in files)
                         {
                             if (!File.Exists(file))
                             {
                                 FileSystem.ErrorWriteLine("File " + file + " dose not exist!");
                                 continue;
                             }
-                            output.AppendLine(LineOutput(file, currentDir, searchString));
+                            string o = LineOutput(file, currentDir, searchString);
+                            output.AppendLine(o);
+                        }
+                    }
+                    var directoryInfo = new DirectoryInfo(currentDir).GetDirectories();
+                    foreach (var dir in directoryInfo)
+                    {
+                        string p = "";
+                        string oD = MultiFileOutput(searchString, dir.FullName, p.Split(' '), savedFile, true, fileName);
+                        if (!string.IsNullOrWhiteSpace(oD))
+                        {
+                            output.AppendLine(oD);
                         }
                     }
                 }
                 else
                 {
-                    foreach (var file in files)
+                    foreach (var file in paths)
                     {
-                        if (!File.Exists(file))
+                        var nFile = FileSystem.SanitizePath(file, currentDir);
+                        if (!File.Exists(nFile))
                         {
-                            FileSystem.ErrorWriteLine("File " + file + " dose not exist!");
+                            FileSystem.ErrorWriteLine("File " + nFile + " dose not exist!");
                             continue;
                         }
-                        string o = LineOutput(file, currentDir, searchString);
-                        output.AppendLine(o);
+                        output.AppendLine(LineOutput(nFile, currentDir, searchString));
                     }
                 }
-                var directoryInfo = new DirectoryInfo(currentDir).GetDirectories();
-                foreach (var dir in directoryInfo)
+
+
+                if (!string.IsNullOrEmpty(savedFile) && searchAll == false)
                 {
-                    string p = "";
-                    string oD = MultiFileOutput(searchString, dir.FullName, p.Split(' '), savedFile, true, fileName);
-                    if (!string.IsNullOrWhiteSpace(oD))
-                    {
-                        output.AppendLine(oD);
-                    }
+                    return FileSystem.SaveFileOutput(savedFile, currentDir, output.ToString());
                 }
-            }
-            else
-            {
-                foreach (var file in paths)
-                {
-                    var nFile = FileSystem.SanitizePath(file, currentDir);
-                    if (!File.Exists(nFile))
-                    {
-                        FileSystem.ErrorWriteLine("File " + nFile + " dose not exist!");
-                        continue;
-                    }
-                    output.AppendLine(LineOutput(nFile, currentDir, searchString));
-                }
-            }
 
-
-            if (!string.IsNullOrEmpty(savedFile) && searchAll == false)
-            {
-                return FileSystem.SaveFileOutput(savedFile, currentDir, output.ToString());
+                return output.ToString();
             }
-
-            return output.ToString();
+            catch (UnauthorizedAccessException) { return ""; }
         }
 
         private static string LineOutput(string file, string currentDir, string searchString)
