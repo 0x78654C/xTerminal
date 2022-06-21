267bb8b845acc36c7c09e5a77dd63c27471a6b65
x_coding <l2xguard@live.com>
2022-06-22 00:59:04 +0300

    set the ability to run alias commands with param

--- Commands/CommandRepository.cs
+++ Commands/CommandRepository.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using Core;
+using Json = Core.SystemTools.JsonManage;
+using AliasC = Core.SystemTools.AliasC;
 
 namespace Commands
 {
     public static class CommandRepository
     {
+        private static string s_aliasFile = GlobalVariables.aliasFile;
+
         private static readonly Dictionary<string, ITerminalCommand> s_terminalCommands =
                     Assembly.GetExecutingAssembly().GetTypes()
                         .Where(t => typeof(ITerminalCommand).IsAssignableFrom(t) && t.IsClass && !t.IsAbstract)
@@ -33,16 +39,44 @@ namespace Commands
                 return null;
             }
 
+            ITerminalCommand terminalCommandOut;
+
             // Get the first word from the parameters. This should be the command name
             string commandName = commandLine.Split().First();
-
-            if (!s_terminalCommands.TryGetValue(commandName, out var terminalCommand)
+            if (!s_terminalCommands.TryGetValue(commandName, out terminalCommandOut)
                 && !s_shellCommands.Contains(commandLine) && !commandLine.StartsWith("cmd") && !commandLine.StartsWith("ps"))
             {
-                Console.WriteLine($"Unknown command: {commandLine}");
+                string alias= GetAliasCommand(commandName, s_aliasFile);
+                if (string.IsNullOrEmpty(alias) || !s_terminalCommands.TryGetValue(alias.Split().First(),out terminalCommandOut))
+                    Console.WriteLine($"Unknown command: {commandLine}");
             }
+            return terminalCommandOut;
+        }
 
-            return terminalCommand;
+        /// <summary>
+        /// Get alias command name and param.
+        /// </summary>
+        /// <param name="commandName"></param>
+        /// <param name="aliasJsonFile"></param>
+        /// <returns></returns>
+        private static string GetAliasCommand(string commandName, string aliasJsonFile)
+        {
+            if (!File.Exists(aliasJsonFile))
+            {
+                FileSystem.ErrorWriteLine("Alias file does not exist!");
+                return string.Empty;
+            }
+            string command = string.Empty;
+            var aliasCommands = Json.ReadJsonFromFile<AliasC[]>(aliasJsonFile);
+            foreach (var alias in aliasCommands)
+            {
+                if (alias.CommandName == commandName)
+                {
+                    command = alias.Command;
+                    GlobalVariables.aliasParameters = command;
+                }
+            }
+            return command;
         }
     }
 }
