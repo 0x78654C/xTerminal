421e94c9a5e7561e42b5672c9fb8f53461bea314
x_coding <l2xguard@live.com>
2022-06-22 15:02:06 +0300

    restored broken file

--- Core/SystemTools/AliasC.cs
+++ Core/SystemTools/AliasC.cs
@@ -1,25 +1,1 @@
-﻿using System.Collections.Generic;
-
-namespace Core.SystemTools
-{
-    public class AliasC
-    {
-        public string CommandName { get; set; }
-        public string Command { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            return obj is AliasC alias &&
-                   CommandName == alias.CommandName &&
-                   Command == alias.Command;
-        }
-
-        public override int GetHashCode()
-        {
-            int hashCode = -1670917873;
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(CommandName);
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Command);
-            return hashCode;
-        }
-    }
-}
+using System.Collections.Generic;namespace Core.SystemTools{    public class AliasC    {        public string CommandName { get; set; }        public string Command { get; set; }        public override bool Equals(object obj)        {            return obj is AliasC alias &&                   CommandName == alias.CommandName &&                   Command == alias.Command;        }        public override int GetHashCode()        {            int hashCode = -1670917873;            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(CommandName);            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Command);            return hashCode;        }    }}
