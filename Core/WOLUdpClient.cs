cf45e67557a8fb5a0641cc2f53267c11ab9f32b7
x_coding <l2xguard@live.com>
2022-08-18 01:10:21 +0300

    added check for UDP brodecast mode

---
+++ Core/WOLUdpClient.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Sockets;
+
+namespace Core
+{
+    public class WOLUdpClient : UdpClient
+    {
+        public WOLUdpClient(): base() { }
+
+        /// <summary>
+        /// Set up the UDP client to broadcast packets.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsClientInBroadcastMode()
+        {
+            bool broadcast = false;
+            if (this.Active)
+            {
+                try
+                {
+                    this.Client.SetSocketOption(SocketOptionLevel.Socket,
+                        SocketOptionName.Broadcast, 0);
+                    broadcast = true;
+                }
+                catch
+                {
+                    broadcast = false;
+                }
+            }
+            return broadcast;
+        }
+    }
+}
