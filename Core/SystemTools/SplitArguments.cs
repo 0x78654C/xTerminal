820cd57854c206cae2eb8ba53134bea1f75228b0
x_coding <l2xguard@live.com>
2022-09-08 00:28:06 +0300

    added class for split string to command line arguments using windows api

---
+++ Core/SystemTools/SplitArguments.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Core.SystemTools
+{
+    public class SplitArguments
+    {
+        /*
+         * Command line parser from string;
+         */
+
+        private string CommandLine { get; set; }
+
+        [DllImport("shell32.dll", SetLastError = true)]
+        static extern IntPtr CommandLineToArgvW([MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine, out int pNumArgs);
+
+        public SplitArguments(string commandLine)
+        {
+            CommandLine = commandLine;
+        }
+
+        /// <summary>
+        /// Convert string to command lines arguments.
+        /// </summary>
+        /// <returns>string[]</returns>
+        public string[] CommandLineToArgs()
+        {
+            int argc;
+            var argv = CommandLineToArgvW(CommandLine, out argc);
+            if (argv == IntPtr.Zero)
+                FileSystem.ErrorWriteLine("Something went wrong. Check parameter!");
+            try
+            {
+                var args = new string[argc];
+                for (var i = 0; i < args.Length; i++)
+                {
+                    var p = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
+                    args[i] = Marshal.PtrToStringUni(p);
+                }
+                return args;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(argv);
+            }
+        }
+    }
+}
