6b79b0d2db853127f5864373234fb6d36fbc3366
x_coding <l2xguard@live.com>
2022-09-08 00:24:04 +0300

    added assambly reference list load for roslyn code runner

--- Core/SystemTools/Roslyn.cs
+++ Core/SystemTools/Roslyn.cs
@@ -1,27 +1,24 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 
 namespace Core.SystemTools
 {
     public  class Roslyn
     {
-        /// <summary>
-        /// Check if file is managed code lib.
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        public static bool IsManaged(string path)
-        {
-            try
-            {
-                var b = AssemblyName.GetAssemblyName(path);
-                return true;
-            }
-            catch
-            {
 
-            }
-            return false;
+        /// <summary>
+        /// Get runtime managed assambly.
+        /// </summary>
+        /// <returns></returns>
+        public static List<MetadataReference> References()
+        {
+            List<MetadataReference> references = new List<MetadataReference>();
+            foreach (var refs in ((string)AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES")).Split(Path.PathSeparator))
+                references.Add(MetadataReference.CreateFromFile(refs));
+            return references;
         }
     }
 }
