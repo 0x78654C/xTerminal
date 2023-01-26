fd56747d37a5a32b85af8560fe96caf55f21c60d
x_coding <l2xguard@live.com>
2023-01-26 22:01:10 +0200

    added command class for file shred

---
+++ Commands/TerminalCommands/ConsoleSystem/FileShred.cs
@@ -0,0 +1,54 @@
+﻿using Core;
+using Core.SystemTools;
+using System;
+using System.IO;
+
+namespace Commands.TerminalCommands.ConsoleSystem
+{
+    public class FileShred : ITerminalCommand
+    {
+        public string Name => "shred";
+        Shred shred;
+        private static string s_helpMessage = @"Usage of shred command:
+  shred <file_path> :   Will shred the file with the default of 3 passes.
+  shred <file_path> -i <number_of_passes> :   Will shred the file with the specified number!
+";
+
+        public void Execute(string args)
+        {
+            try
+            {
+                if (args.Length == 5)
+                {
+                    Console.WriteLine($"Use -h param for {Name} command usage!");
+                    return;
+                }
+                if (args == $"{Name} -h")
+                {
+                    Console.WriteLine(s_helpMessage);
+                    return;
+                }
+
+                args = args.Replace("shred ", String.Empty);
+                var currentDirectory = File.ReadAllText(GlobalVariables.currentDirectory);
+
+                if (args.Contains(" -i "))
+                {
+                    int passes = Int32.Parse(args.SplitByText("-i ", 1).Trim());
+                    string filePath = args.SplitByText(" -i", 0).Trim();
+                    string fileSanitizeI = FileSystem.SanitizePath(filePath, currentDirectory);
+                    shred = new Shred(fileSanitizeI, passes);
+                    shred.ShredFile();
+                    return;
+                }
+                string fileSanitize = FileSystem.SanitizePath(args, currentDirectory);
+                shred = new Shred(fileSanitize, 0);
+                shred.ShredFile();
+            }
+            catch (Exception ex)
+            {
+                FileSystem.ErrorWriteLine($"{ex.Message}\nUse -h param for {{Name}} command usage!");
+            }
+        }
+    }
+}
