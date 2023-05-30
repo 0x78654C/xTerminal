67428678eae195f23ba5351ec9bd6b6262b415de
x_Coding <xcoding.dev@gmail.com>
2023-05-30 23:58:40 +0300

    create class for new dspoof command

---
+++ Commands/TerminalCommands/Network/SpoofDetect.cs
@@ -0,0 +1,21 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commands.TerminalCommands.Network
+{
+    [SupportedOSPlatform("Windows")]
+    public class SpoofDetect : ITerminalCommand
+    {
+        public string Name  => "dspoof";
+        public void Execute(string arg)
+        {
+            var gateway = NetWork.GetGetewayIp();
+            Console.WriteLine(gateway);
+        }
+    }
+}
