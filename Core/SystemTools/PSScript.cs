a99b2ae534690ef4a46b40628d7d2b8ed98d1aa9
x_Coding <xcoding.dev@gmail.com>
2023-05-31 23:06:58 +0300

    added class for run powershell scripts

---
+++ Core/SystemTools/PSScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+
+namespace Core.SystemTools
+{
+    public class PSScript
+    {
+
+        /// <summary>
+        /// Running powershell scripts.
+        /// </summary>
+        /// <param name="scriptText"></param>
+        /// <returns></returns>
+        public static string RunScript(string scriptText)
+        {
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+            Pipeline pipeline = runspace.CreatePipeline();
+            pipeline.Commands.AddScript(scriptText);
+            pipeline.Commands.Add("Out-String");
+            Collection<PSObject> results = pipeline.Invoke();
+            runspace.Close();
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (PSObject obj in results)
+            {
+                stringBuilder.AppendLine(obj.ToString());
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
