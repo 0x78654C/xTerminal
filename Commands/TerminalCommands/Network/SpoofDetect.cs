48469ca247630df6c0712533e766a021b9996f30
x_Coding <xcoding.dev@gmail.com>
2023-06-01 22:14:41 +0300

    changed UI for display attacker info and added help message

--- Commands/TerminalCommands/Network/SpoofDetect.cs
+++ Commands/TerminalCommands/Network/SpoofDetect.cs
@@ -1,25 +1,36 @@
 ﻿using Core;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Versioning;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Commands.TerminalCommands.Network
 {
     [SupportedOSPlatform("Windows")]
     public class SpoofDetect : ITerminalCommand
     {
+        private static string s_helpMessage = @"The command detects MITM(man in the middle) attacks using ARP spoof method. Here is a short description:
+
+ARP spoofing refers to an attacker with access to the LAN pretending to be Host B. 
+The attacker sends messages to Host A with the goal of tricking Host A into saving the attacker’s address as Host B’s address. 
+Host A will ultimately send communications intended for Host B to the attacker instead. 
+Once the attacker becomes this middle man, each time Host A communicates with Host B, that host will in fact be communicating first with the attacker.
+Host B will typically be the default gateway, or the router.
+   
+";
         public string Name  => "dspoof";
-        public void Execute(string arg)
+        public void Execute(string args)
         {
+            if (args == $"{Name} -h")
+            {
+                Console.WriteLine(s_helpMessage);
+                return;
+            }
+
+            // Run MITM ARP spoof detection.
             CheckSpoof();
         }
 
         /// <summary>
-        /// Run MITM ARP spoof check.
+        /// MITM ARP spoof check.
         /// </summary>
         private void CheckSpoof()
         {
@@ -27,8 +38,9 @@ namespace Commands.TerminalCommands.Network
             var gatewayIP = NetWork.GetGetewayIp();
             var gatewayMAC = NetWork.GetMacAddress(gatewayIP);
             var arpTable = NetWork.GetIPsAndMac(localIP);
+            bool isAttack = false;
             int skip = 0;
-            DisplayUI(gatewayIP, gatewayMAC);
+            DisplayUIMain(gatewayIP, gatewayMAC);
             foreach (var arp in arpTable)
             {
                 if(skip >= 1)
@@ -36,20 +48,23 @@ namespace Commands.TerminalCommands.Network
                     if(arp.MAC == gatewayMAC)
                     {
                         FileSystem.ColorConsoleTextLine(ConsoleColor.Red, "ARP spoof detected from:");
-                        Console.WriteLine($"{arp.IP} | {arp.MAC}");
+                        DisplayUIAttack(arp.IP, arp.MAC);
+                        isAttack = true;
                         break;
                     }
                 }
                 skip++;
             }
+            if (!isAttack)
+                FileSystem.ColorConsoleTextLine(ConsoleColor.Green, "Seems all good!");
         }
 
         /// <summary>
-        /// Display UI stuff.
+        /// Display MAIN UI stuff.
         /// </summary>
         /// <param name="gatewayIP"></param>
         /// <param name="gatewayMAC"></param>
-        public void DisplayUI(string gatewayIP, string gatewayMAC)
+        public void DisplayUIMain(string gatewayIP, string gatewayMAC)
         {
             FileSystem.ColorConsoleTextLine(ConsoleColor.Green, "-------------------------------------");
             FileSystem.ColorConsoleText(ConsoleColor.Green, "Gateway IP: ");
@@ -57,6 +72,19 @@ namespace Commands.TerminalCommands.Network
             FileSystem.ColorConsoleText(ConsoleColor.Green, "Gateway MAC: ");
             Console.WriteLine(gatewayMAC);
             FileSystem.ColorConsoleTextLine(ConsoleColor.Green, "-------------------------------------");
+        }
+
+        /// <summary>
+        /// Display UI stuff.
+        /// </summary>
+        /// <param name="gatewayIP"></param>
+        /// <param name="gatewayMAC"></param>
+        public void DisplayUIAttack(string ipAddress, string macAddress)
+        {
+            FileSystem.ColorConsoleText(ConsoleColor.Yellow, "IP: ");
+            Console.Write(ipAddress);
+            FileSystem.ColorConsoleText(ConsoleColor.Yellow, " | MAC: ");
+            Console.WriteLine(macAddress);
         }
     }
 }
