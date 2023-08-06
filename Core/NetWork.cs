a1424a120e323859f87dfe52d27788362522584c
xeL <l2xguard@live.com>
2023-08-06 22:45:46 +0300

    cleanup

--- Core/NetWork.cs
+++ Core/NetWork.cs
@@ -1,17 +1,13 @@
 ﻿using Core.SystemTools;
 using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
-using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Core
 {
