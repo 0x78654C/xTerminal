3716d385af93e9840e2be965efae647d175651b8
xeL <l2xguard@live.com>
2023-09-02 22:07:56 +0300

    modified help to inform witch parameter works with pipe commands on cat

--- Commands/TerminalCommands/DirFiles/StringView.cs
+++ Commands/TerminalCommands/DirFiles/StringView.cs
@@ -18,13 +18,13 @@ namespace Commands.TerminalCommands.DirFiles
         private static string s_output = string.Empty;
         private static string s_helpMessage = @"Usage of cat command:
   -h   : Displays this message.
-  -n   : Displays first N lines from a file.
+  -n   : Displays first N lines from a file. Works with pipe commands.
            Example: cat -n 10 <path_of_file_name>
-  -l   : Displays data between two lines range.
+  -l   : Displays data between two lines range. Works with pipe commands.
            Example: cat -l 10-20 <path_of_file_name>
-  -s   : Outputs lines containing a provided text from a file.
+  -s   : Outputs lines containing a provided text from a file. Works with pipe commands.
            Example: cat -s <search_text> -f <file_search_in>
-  -so  : Saves the lines containing a provided text from a file.
+  -so  : Saves the lines containing a provided text from a file. Works with pipe commands.
            Example: cat -so <search_text> -f <file_search_in> -o <file_to_save>
   -sa  : Output lines containing a provided text from all files in current directory and subdirectories.
            Example1: cat -sa <search_text>
