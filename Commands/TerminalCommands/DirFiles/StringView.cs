118bb2807c521c1e1821b4e28f16dc2471f5e06e
xeL <l2xguard@live.com>
2023-09-22 21:14:04 +0300

    removed clear of pipe command

--- Commands/TerminalCommands/DirFiles/StringView.cs
+++ Commands/TerminalCommands/DirFiles/StringView.cs
@@ -174,7 +174,9 @@ Commands can be canceled with CTRL+X key combination.
                             searchString = arg.SplitByText("-s ", 1);
                             GlobalVariables.eventKeyFlagX = true;
                             if (GlobalVariables.isPipeCommand && GlobalVariables.pipeCmdCount > 0 && GlobalVariables.pipeCmdCount < GlobalVariables.pipeCmdCountTemp)
+                            {
                                 GlobalVariables.pipeCmdOutput = Core.Commands.CatCommand.StringSearchOutput(GlobalVariables.pipeCmdOutput, s_currentDirectory, searchString, "");
+                            }
                             else
                                 Console.WriteLine(Core.Commands.CatCommand.StringSearchOutput(GlobalVariables.pipeCmdOutput, s_currentDirectory, searchString, ""));
                             if (GlobalVariables.eventCancelKey)
