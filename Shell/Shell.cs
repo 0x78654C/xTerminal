17dbb0e7a23b0aff37d36a59793aebabb064d06f
xeL <l2xguard@live.com>
2023-10-15 20:37:21 +0300

    clean unused code

--- Shell/Shell.cs
+++ Shell/Shell.cs
@@ -130,9 +130,6 @@ namespace Shell
                         {
                             var cmdExecute = cmd.Trim();
                             c = Commands.CommandRepository.GetCommand(cmdExecute);
-
-                          //  if (count == 0) deactivated temporary  
-                            //    GlobalVariables.pipeCmdOutput = cmdExecute;
                              
                             c.Execute(cmdExecute);
                             
