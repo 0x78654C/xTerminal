2365017f3e754a9a28fe758d5a669a8a4dcdb94f
xeL <xcoding.dev@gmail.com>
2024-01-14 10:38:49 +0200

    wip : new command created for list processes tree

---
+++ Commands/TerminalCommands/ConsoleSystem/ProcessList.cs
@@ -0,0 +1,41 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using Core.SystemTools;
+namespace Commands.TerminalCommands.ConsoleSystem
+{
+    [SupportedOSPlatform("Windows")]
+    public class ProcessList : ITerminalCommand
+    {
+
+        private static string s_currentDirectory = string.Empty;
+        public string Name => "plist";
+        private string _helpMessage = @"TODO: work in progress.
+";
+
+
+        public void Execute(string args)
+        {
+            try
+            {
+                if (args.Length == 5)
+                //{
+                //    Console.WriteLine($"Use -h param for {Name} command usage!");
+                //    return;
+                //}
+
+                // Main run
+                ListProcesses.GetProcessList();
+
+            }catch(Exception e)
+            {
+                FileSystem.ErrorWriteLine(e.Message); // We live like this till finish command.
+            }
+        }
+
+    }
+}
---
+++ Core/SystemTools/ListProcesses.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Runtime.Versioning;
+using System.Diagnostics;
+namespace Core.SystemTools
+{
+    [SupportedOSPlatform("Windows")]
+    public class ListProcesses
+    {
+        private static List<string> processList = new List<string> { };
+        public static void GetProcessList()
+        {
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_Process"))
+            {
+                foreach (var process in searcher.Get())
+                {
+                    int id = Int32.Parse(process["ProcessID"].ToString());
+                    string processName = Process.GetProcessById(id).ProcessName;
+                    string item = $"Parent: [{processName}] [{id}]";
+                    if (!processList.Contains(item))
+                        processList.Add(item);
+                    GetChildProcesses(id);
+                }
+            }
+            string outList = string.Join("\n", processList);
+            Console.WriteLine(outList);
+        }
+
+        private static void GetChildProcesses(int id)
+        {
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_Process Where ParentProcessId=" + id))
+            {
+                foreach (var process in searcher.Get())
+                {
+                    int idChildProcess = Int32.Parse(process["ProcessID"].ToString());
+                    string processName = Process.GetProcessById(idChildProcess).ProcessName;
+                    string item = $"    [{processName}] [{idChildProcess}]";
+                    foreach(var i in processList)
+                    {
+                        if (i.Contains($"Parent: [{processName}] [{idChildProcess}]"));
+                        processList.Remove(i);
+                    }
+
+                    if (!processList.Contains(item))
+                        processList.Add(item);
+                }
+            }
+        }
+    }
+}
