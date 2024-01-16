0f44ca60420e2b7b159b4e4eb75d09defa33425b
xeL <xcoding.dev@gmail.com>
2024-01-16 23:12:15 +0200

    more plist tests

--- Core/SystemTools/ListProcesses.cs
+++ Core/SystemTools/ListProcesses.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Management;
 using System.Runtime.Versioning;
 using System.Diagnostics;
@@ -12,6 +10,7 @@ namespace Core.SystemTools
     public class ListProcesses
     {
         private static List<string> processList = new List<string> { };
+        private static List<string> childProcess = new List<string> { };
         public static void GetProcessList()
         {
             using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_Process"))
@@ -21,12 +20,24 @@ namespace Core.SystemTools
                     int id = Int32.Parse(process["ProcessID"].ToString());
                     string processName = Process.GetProcessById(id).ProcessName;
                     string item = $"Parent: [{processName}] [{id}]";
-                    if (!processList.Contains(item))
-                        processList.Add(item);
+
                     GetChildProcesses(id);
+                    if (!processList.Contains(item))
+                            processList.Add(item);
                 }
             }
+
+  
             string outList = string.Join("\n", processList);
+            foreach (var item in processList)
+            {
+                if (item.StartsWith("Parent"))
+                {
+                    var itemTrim = item.Replace("Parent: ", "    ");
+                    if(outList.Contains(itemTrim))
+                        outList = outList.Replace(item, "\r\n");
+                }
+            }
             Console.WriteLine(outList);
         }
 
@@ -39,14 +50,9 @@ namespace Core.SystemTools
                     int idChildProcess = Int32.Parse(process["ProcessID"].ToString());
                     string processName = Process.GetProcessById(idChildProcess).ProcessName;
                     string item = $"    [{processName}] [{idChildProcess}]";
-                    foreach(var i in processList)
-                    {
-                        if (i.Contains($"Parent: [{processName}] [{idChildProcess}]"));
-                        processList.Remove(i);
-                    }
 
-                    if (!processList.Contains(item))
-                        processList.Add(item);
+                        if (!processList.Contains(item))
+                            processList.Add(item);
                 }
             }
         }
