5a6a91408de4aa198fd351befce8ec917f587de7
xeL <xcoding.dev@gmail.com>
2024-01-18 22:55:47 +0200

    progress on listing tree

--- Core/SystemTools/ListProcesses.cs
+++ Core/SystemTools/ListProcesses.cs
@@ -4,56 +4,46 @@ using System.Linq;
 using System.Management;
 using System.Runtime.Versioning;
 using System.Diagnostics;
+using System.Threading;
 namespace Core.SystemTools
 {
     [SupportedOSPlatform("Windows")]
     public class ListProcesses
     {
-        private static List<string> processList = new List<string> { };
-        private static List<string> childProcess = new List<string> { };
+        private static List<string> s_processList = new List<string> { };
         public static void GetProcessList()
         {
-            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_Process"))
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_Process");
+            foreach (var process in searcher.Get())
             {
-                foreach (var process in searcher.Get())
+                int id = Int32.Parse(process["ProcessID"].ToString());
+                string processName = Process.GetProcessById(id).ProcessName;
+                string item = $"Parent : [{processName}] [{id}]";
+                string searchPattern = $"    [{processName}] [{id}]";
+                if (s_processList.Any(i => i.Contains(searchPattern)))
                 {
-                    int id = Int32.Parse(process["ProcessID"].ToString());
-                    string processName = Process.GetProcessById(id).ProcessName;
-                    string item = $"Parent: [{processName}] [{id}]";
-
-                    GetChildProcesses(id);
-                    if (!processList.Contains(item))
-                            processList.Add(item);
+                    int countExisting = s_processList.Where(i => i.Contains(searchPattern)).Count();
+                    item = $"Parent ({countExisting}) : [{processName}] [{id}]";
                 }
+
+                if (!s_processList.Contains(item))
+                    s_processList.Add(item);
+                GetChildProcesses(id);
             }
 
-  
-            string outList = string.Join("\n", processList);
-            foreach (var item in processList)
-            {
-                if (item.StartsWith("Parent"))
-                {
-                    var itemTrim = item.Replace("Parent: ", "    ");
-                    if(outList.Contains(itemTrim))
-                        outList = outList.Replace(item, "\r\n");
-                }
-            }
+            string outList = string.Join("\n", s_processList);
             Console.WriteLine(outList);
         }
-
         private static void GetChildProcesses(int id)
         {
-            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_Process Where ParentProcessId=" + id))
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_Process Where ParentProcessId=" + id);
+            foreach (var process in searcher.Get())
             {
-                foreach (var process in searcher.Get())
-                {
-                    int idChildProcess = Int32.Parse(process["ProcessID"].ToString());
-                    string processName = Process.GetProcessById(idChildProcess).ProcessName;
-                    string item = $"    [{processName}] [{idChildProcess}]";
-
-                        if (!processList.Contains(item))
-                            processList.Add(item);
-                }
+                int idChildProcess = Int32.Parse(process["ProcessID"].ToString());
+                string processName = Process.GetProcessById(idChildProcess).ProcessName;
+                string item = $"    [{processName}] [{idChildProcess}]";
+                if (!s_processList.Contains(item))
+                    s_processList.Add(item);
             }
         }
     }
