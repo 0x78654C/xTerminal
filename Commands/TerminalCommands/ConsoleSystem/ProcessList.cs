f059402948dcf9a18bc7e14600f57acbe532e2d0
xeL <xcoding.dev@gmail.com>
2024-01-20 21:40:10 +0200

    added help message and fixed -h command

--- Commands/TerminalCommands/ConsoleSystem/ProcessList.cs
+++ Commands/TerminalCommands/ConsoleSystem/ProcessList.cs
@@ -1,45 +1,44 @@
 ﻿using Core;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Versioning;
-using System.Text;
-using System.Threading.Tasks;
 using Core.SystemTools;
 namespace Commands.TerminalCommands.ConsoleSystem
 {
     [SupportedOSPlatform("Windows")]
     public class ProcessList : ITerminalCommand
     {
+        /*
+         Get process tree.
+        */
 
-        private static string s_currentDirectory = string.Empty;
         public string Name => "plist";
-        private string _helpMessage = @"List current running processes and their child process.
+        private string _helpMessage = @"List current running processes and their child processes.
+Example: 
+    Parent : [Idle] [0]   ---> parent process
+        [Idle] [0]        ---> child process
+        [System] [4]      ---> child process
+    Parent (1) : [System] ---> parent process(was child process for parent process '[Idle] [0]')
+        [Registry] [132]  ---> child process
+
 ";
 
-
+        // Main run
         public void Execute(string args)
         {
             try
             {
-                if (args == "-h")
+                if (args == $"{Name} -h")
                 {
                     Console.WriteLine(_helpMessage);
                     return;
                 }
-                if (args.Length == 5)
-                //{
-                //    Console.WriteLine($"Use -h param for {Name} command usage!");
-                //    return;
-                //}
-
-                // Main run
 
                 ListProcesses.GetProcessList();
 
-            }catch(Exception e)
+            }
+            catch (Exception e)
             {
-                FileSystem.ErrorWriteLine(e.ToString()); // We live like this till finish command.
+                FileSystem.ErrorWriteLine(e.Message);
             }
         }
 
