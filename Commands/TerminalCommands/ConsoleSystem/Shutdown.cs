0d4933a430e2dde5a334d68017ef64d9334a920e
x_coding <xcoding.dev@gmail.com>
2024-08-16 15:36:17 +0300

    created shutdown class command

---
+++ Commands/TerminalCommands/ConsoleSystem/Shutdown.cs
@@ -0,0 +1,53 @@
+﻿/*
+ *
+ * shutdown command.
+ * 
+ */
+
+
+using System;
+using System.Runtime.Versioning;
+using Core;
+using Core.Commands;
+
+namespace Commands.TerminalCommands.ConsoleSystem
+{
+    [SupportedOSPlatform("Windows")]
+    public class Shutdown : ITerminalCommand
+    {
+        public string Name => "shutdown";
+        private static string s_messageHelp = @"Usage of shutdown command:
+    shutdown    : shutdown system normaly.
+    shutdown -f : force shutdown system;
+";
+
+        public void Execute(string arg)
+        {
+            try
+            {
+                if (arg == "shutdown")
+                {
+                    SystemCommands.ShutDownCmd(false);
+                    return;
+                }
+
+                var split = arg.Split(' ');
+                if (split[1].Trim() == "-f")
+                {
+                    SystemCommands.ShutDownCmd(true);
+                    return;
+                }
+
+                if (split[1].Trim() == "-h")
+                {
+                    Console.WriteLine(s_messageHelp);
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                FileSystem.ErrorWriteLine($"{e.Message}. Use -h for more information!");
+            }
+        }
+    }
+}
