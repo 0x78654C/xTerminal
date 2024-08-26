dc2b777337febc8b61432383e0423583edaa3d27
xeL <xcoding.dev@gmail.com>
2024-08-26 23:26:40 +0300

    created time command for display currentt time

---
+++ Commands/TerminalCommands/ConsoleSystem/Time.cs
@@ -0,0 +1,21 @@
+﻿using Core;
+using System;
+using System.Runtime.Versioning;
+
+namespace Commands.TerminalCommands.ConsoleSystem
+{
+    /*
+        Display current time.
+    */
+
+    [SupportedOSPlatform("Windows")]
+    public class Time: ITerminalCommand
+    {
+        public string Name => "time";
+        public void Execute(string arg)
+        {
+            var now = DateTime.Now.ToString("HH:mm:ss dd-MM-yyyy");
+            FileSystem.SuccessWriteLine(now); 
+        }
+    }
+}
