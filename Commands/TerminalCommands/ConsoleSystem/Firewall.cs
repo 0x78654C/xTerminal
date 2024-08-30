0ae7a5e1a5bc2a8f34cd35d798d8c3e8bca751e4
xeL <xcoding.dev@gmail.com>
2024-08-30 23:44:33 +0300

    created firewall manage command: fw

---
+++ Commands/TerminalCommands/ConsoleSystem/Firewall.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Core;
+using Core.SystemTools;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Versioning;
+
+namespace Commands.TerminalCommands.ConsoleSystem
+{
+    [SupportedOSPlatform("Windows")]
+    public class Firewall: ITerminalCommand
+    {
+        public string Name => "fw";
+        public void Execute(string arg)
+        {
+            var fw = new FirewallManager();
+            fw.Port = "2122";
+            fw.RuleName = "testRule";
+            fw.AddRule(FirewallManager.Action.Allow, FirewallManager.Protocol.TCP);
+        }
+    }
+}
