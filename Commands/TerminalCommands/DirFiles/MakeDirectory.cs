a3d7743b1e5c026794f6ae55763aa7ef2863ea8b
xeL <xcoding.dev@gmail.com>
2024-10-04 00:04:42 +0300

    added help message

--- Commands/TerminalCommands/DirFiles/MakeDirectory.cs
+++ Commands/TerminalCommands/DirFiles/MakeDirectory.cs
@@ -10,6 +10,11 @@ namespace Commands.TerminalCommands.DirFiles
     public class MakeDirectory : ITerminalCommand
     {
         public string Name => "mkdir";
+        private string s_helpMessage = @"Usage of mkdir command:
+    mkdir <dir_name>
+    mkdir <dir_name1;dir_name2;dir_name3>
+";
+
         public void Execute(string arg)
         {
             try
@@ -17,7 +22,12 @@ namespace Commands.TerminalCommands.DirFiles
                 int argLength = arg.Length - 6;
 
                 string input = arg.Substring(6, argLength);
-                string currentDir = File.ReadAllText(GlobalVariables.currentDirectory); ; // Get the new location
+                if (input == $"{Name} -h")
+                {
+                    Console.WriteLine(s_helpMessage);
+                    return;
+                }
+                string currentDir = File.ReadAllText(GlobalVariables.currentDirectory);  // Get the new location
                 var makeDirectory = new DirectoryMake(input, currentDir);
                 makeDirectory.Create();
             }
