a9255c1ad3fa2a324c388bafe8c9004c4b490c2e
xeL <xcoding.dev@gmail.com>
2024-12-09 23:31:36 +0200

    created new command ln for creating shortcuts

---
+++ Commands/TerminalCommands/ConsoleSystem/Shortcut.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commands.TerminalCommands.ConsoleSystem
+{
+    [SupportedOSPlatform("Windows")]
+    public class Shortcut: ITerminalCommand
+    {
+        public string Name => "ls";
+        public void Execute(string args)
+        {
+
+        }
+    }
+}
