91539a1ce6de5b335f665b85046abf32ad8ce6c0
xeL <xcoding.dev@gmail.com>
2024-12-09 23:31:45 +0200

    added shortcut maker

---
+++ Core/SystemTools/ShortcutMaker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using IWshRuntimeLibrary;
+
+namespace Core.SystemTools
+{
+    [SupportedOSPlatform("Windows")]
+    public class ShortcutMaker
+    {
+         public ShortcutMaker() { }
+        void CreateShortcut(string sendToPath, string fullPath)
+        {
+            //Environment.SpecialFolder.Desktop
+            string fileName = Path.GetFileNameWithoutExtension(fullPath);
+            IWshShortcut shortcut;
+            if (Environment.Is64BitOperatingSystem)
+            {
+                WshShell wshShell = new WshShell();
+                shortcut = (IWshShortcut)wshShell.CreateShortcut(Path.Combine(sendToPath, fileName + ".lnk"));
+            }
+            else
+            {
+                WshShellClass wshShell = new WshShellClass();
+                shortcut = (IWshShortcut)wshShell.CreateShortcut(Path.Combine(sendToPath, fileName + ".lnk"));
+            }
+            shortcut.TargetPath = fullPath;
+            shortcut.IconLocation = fullPath;
+            shortcut.Save();
+        }
+    }
+}
