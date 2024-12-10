6e39ca305448e5dcb19ed42c214b906e161c52b7
x_coding <xcoding.dev@gmail.com>
2024-12-10 20:58:06 +0200

    created ln command for shortcut creation

--- Commands/TerminalCommands/ConsoleSystem/Shortcut.cs
+++ Commands/TerminalCommands/ConsoleSystem/Shortcut.cs
@@ -1,19 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Core;
+using Core.SystemTools;
+using System;
+using System.IO;
 using System.Runtime.Versioning;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Commands.TerminalCommands.ConsoleSystem
 {
     [SupportedOSPlatform("Windows")]
-    public class Shortcut: ITerminalCommand
+    public class Shortcut : ITerminalCommand
     {
         public string Name => "ln";
-        public void Execute(string args)
+        private static string s_helpMessage = @"Usage of ln command parameters:
+    ln <path_file_folder> : Create shortcut of a specific file/directory on Desktop.
+    ln <path_file_folder> -o <path_location_shortcut> : Create shortcut in a specific location.
+";
+        public void Execute(string arg)
         {
+            try
+            {
 
+                // No parameter.
+                if (arg == Name)
+                {
+                    FileSystem.SuccessWriteLine("Use -h for more information!");
+                    return;
+                }
+
+                arg = arg.Substring(2);
+
+                // Display help message.
+                if (arg.Trim() == "-h")
+                {
+                    Console.WriteLine(s_helpMessage);
+                    return;
+                }
+                var shortcutMaker =  new ShortcutMaker();
+
+                // Save shortcut to specific directory.
+                if(arg.Contains(" -o "))
+                {
+                    var fileShort = arg.SplitByText(" -o ", 0).Trim();
+                    var pathShort = arg.SplitByText(" -o ", 1).Trim();
+                    shortcutMaker.PathShortcut = pathShort;
+                    shortcutMaker.Path = fileShort;
+                    shortcutMaker.SaveDesktop = false;
+                    shortcutMaker.CreateShortcut();
+                    return;
+                }
+                shortcutMaker.Path = arg.Trim();
+                shortcutMaker.CreateShortcut();
+            }
+            catch (Exception ex)
+            {
+                FileSystem.ErrorWriteLine($"{ex.Message}. Use -h for more information!");
+            }
         }
     }
 }
