aed8e67eac4cb2548199ccbade02c314d2d63e94
x_coding <xcoding.dev@gmail.com>
2024-12-10 23:22:43 +0200

    created blue print for zip command

---
+++ Commands/TerminalCommands/ConsoleSystem/Zip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commands.TerminalCommands.ConsoleSystem
+{
+    public class Zip : ITerminalCommand
+    {
+        public string Name => "zip";
+        private static string s_helpMessage = @"Usage of ln command parameters:
+    ln <path_file_folder> : Create shortcut of a specific file/directory on Desktop.
+    ln <path_file_folder> -o <path_location_shortcut> : Create shortcut in a specific location.
+";
+        public void Execute(string arg)
+        {
+        }
+    }
+}
