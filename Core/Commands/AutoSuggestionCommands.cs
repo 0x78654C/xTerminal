722d588ef03f103140d36990e631f80ae3002cca
x_coding <xcoding.dev@gmail.com>
2024-12-16 23:07:30 +0200

    added auto suggestion cmd tool

--- Core/Commands/AutoSuggestionCommands.cs
+++ Core/Commands/AutoSuggestionCommands.cs
@@ -15,39 +15,41 @@ namespace Core.Commands
         /// <param name="command">Command you use.</param>
         /// <param name="currentDirectory">Current directory.</param>
         /// <param name="isFile">If sugestion is for file.</param>
-        public static void FileDirSuggestion(string consoleInput, string command, string currentDirectory, bool isFile)
+        public static void FileDirSuggestion(string consoleInput, string command, string currentDirectory, bool isFile, ref string addedCompletion)
         {
             try
             {
-                if (consoleInput.StartsWith(".-"))
-                    consoleInput = consoleInput.Replace(".-", "./");
-
                 int commandLenght = command.Length + 1;
                 if (consoleInput == command)
                 {
                     GlobalVariables.autoSuggestion = true;
                     GlobalVariables.commandOut = command;
                     Console.WriteLine("\r\n For auto suggestion use: command<SPACE KEY>start characters of files/directories");
-                    SendKeys.Send("{ENTER}");
-                    SendKeys.Send(consoleInput);
+                    SendKeys.SendWait("{ENTER}");
+                    SendKeys.SendWait(consoleInput);
                 }
                 if ((consoleInput.StartsWith(command) && consoleInput.Length > command.Length))
                 {
                     if (isFile)
                     {
-                        GlobalVariables.autoSuggestion = true;
                         consoleInput = consoleInput.Substring(commandLenght, consoleInput.Length - commandLenght);
-                        SystemTools.AutoSuggestion.FileCompletion(consoleInput, currentDirectory);
-                        consoleInput = consoleInput.Replace(".-", "./");
+                        SystemTools.AutoSuggestion.FileCompletion(consoleInput, currentDirectory, ref addedCompletion);
                         GlobalVariables.commandOut = command + " " + consoleInput;
-                        SendKeys.Send(command + " " + consoleInput);
+                        if (string.IsNullOrEmpty(addedCompletion))
+                        {
+                            GlobalVariables.autoSuggestion = true;
+                            SendKeys.SendWait(command + " " + consoleInput);
+                        }
                         return;
                     }
-                    GlobalVariables.autoSuggestion = true;
                     consoleInput = consoleInput.Substring(commandLenght, consoleInput.Length - commandLenght);
-                    SystemTools.AutoSuggestion.DirCompletion(consoleInput, currentDirectory);
+                    SystemTools.AutoSuggestion.DirCompletion(consoleInput, currentDirectory, ref addedCompletion);
                     GlobalVariables.commandOut = command + " " + consoleInput;
-                    SendKeys.Send(command + " " + consoleInput);
+                    if (string.IsNullOrEmpty(addedCompletion))
+                    {
+                        GlobalVariables.autoSuggestion = true;
+                        SendKeys.SendWait(command + " " + consoleInput);
+                    }
                 }
             }
             catch { }
