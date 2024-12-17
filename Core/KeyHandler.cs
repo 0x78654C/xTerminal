fa7dad89a3f5d5b30fffa06fd0bb019b93114dc0
x_coding <xcoding.dev@gmail.com>
2024-12-17 22:11:43 +0200

    removed unused code

--- Core/KeyHandler.cs
+++ Core/KeyHandler.cs
@@ -338,11 +338,6 @@ namespace Core
                     var outCompletion = "";
                     var currentDirectory = File.ReadAllText(GlobalVariables.currentDirectory);
                     var candidate = _text.ToString();
-                    //var candidate = GlobalVariables.currentCommand.Trim('\t');
-                    //candidate = candidate.Trim('\b');
-                    //candidate = candidate.Trim('\0');
-                    //candidate = candidate.Trim('\r');
-                    //candidate = candidate.Trim('\n');
                     AutoSuggestionCommands.FileDirSuggestion(candidate, "cd", currentDirectory, false, ref outCompletion);
                     AutoSuggestionCommands.FileDirSuggestion(candidate, "odir", currentDirectory, false, ref outCompletion);
                     AutoSuggestionCommands.FileDirSuggestion(candidate, "ls", currentDirectory, false, ref outCompletion);
