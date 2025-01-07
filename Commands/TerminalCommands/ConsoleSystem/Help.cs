34ba772643f03e0d6f88e9795bd422b6fa9f62c4
x_coding <xcoding.dev@gmail.com>
2025-01-07 23:01:42 +0200

    updated help with tee command

--- Commands/TerminalCommands/ConsoleSystem/Help.cs
+++ Commands/TerminalCommands/ConsoleSystem/Help.cs
@@ -57,6 +57,7 @@ This is the full list of commands that can be used in xTerminal:
     enc       -- Set input/output encoding for xTerminal. Use -h for additional help.
     ln        -- Create shortcut of a file/folder. Use -h for additional help.
     zip       -- Create Zip archive files. Use -h for additional help.
+    tee       -- Stores previous pipe command stdout to a file. Use -h for additional help. 
 
     ---------------------- File System ---------------------
     cat       -- Displays the content of a file. Use -h for additional parameters.
