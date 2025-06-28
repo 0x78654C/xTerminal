429e73553b44842e7f0de8267c582e2b1efa5441
xeL <xcoding.dev@gmail.com>
2025-06-28 13:08:00 +0300

    ref

--- Core/GlobalVariables.cs
+++ Core/GlobalVariables.cs
@@ -26,7 +26,7 @@ namespace Core
         public static int historyLimitSize { get; set; } = 2000;
         public static string commandOut { get; set; }
         public static string version { get; set; }
-        public static string aliasParameters = string.Empty;
+        public static string aliasParameters { get; set; } = string.Empty;
         public static readonly string accountName = Environment.UserName;
         public static string rootPath = $"{Path.GetPathRoot(Environment.SystemDirectory)}Users\\{accountName}\\";
         public static readonly string computerName = Environment.MachineName;
