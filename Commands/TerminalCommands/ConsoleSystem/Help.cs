406de43c196b9488b5a36a40960206f9840e3922
xeL <xcoding.dev@gmail.com>
2025-06-28 13:21:18 +0300

    added new command: hash

--- Commands/TerminalCommands/ConsoleSystem/Help.cs
+++ Commands/TerminalCommands/ConsoleSystem/Help.cs
@@ -59,6 +59,7 @@ This is the full list of commands that can be used in xTerminal:
     zip       -- Create Zip archive files. Use -h for additional help.
     tee       -- Stores previous pipe command stdout to a file. Use -h for additional help. 
     bc        -- Display running background commands. Use -h for additional help.
+    hash      -- Display the MD5, SHA256 and SHA512 hash of a file. Use -h for additional help.
 
     ---------------------- File System ---------------------
     cat       -- Displays the content of a file. Use -h for additional parameters.
