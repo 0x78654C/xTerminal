224d258462f2af8c4ada3f122e585958e1cb5fab
xeL <xcoding.dev@gmail.com>
2025-08-03 22:46:46 +0300

    adde license

--- xUninstaller/Program.cs
+++ xUninstaller/Program.cs
@@ -1,8 +1,18 @@
 ﻿/*
- 
-xTerminal uninstaller.
- 
+      Description: xTerminal uninstaller
+
+      This app is distributed under the MIT License.
+      Copyright © 2022 - 2025 x_coding. All rights reserved.
+
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+      SOFTWARE.
 */
+
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 namespace xUninstaller
