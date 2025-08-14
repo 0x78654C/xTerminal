46e0f1bba6837881f3cafc41ba3e8d06250b3cba
xeL <xcoding.dev@gmail.com>
2025-08-15 00:15:33 +0300

    added class for process check and kill

---
+++ xInstaller/Utils/ProcessManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace xInstaller
+{
+    internal class ProcessManager
+    {
+
+        /// <summary>
+        /// CTor for process kill.
+        /// </summary>
+        public ProcessManager() { }
+
+        /// <summary>
+        /// Kill processes by name or id.
+        /// </summary>
+        /// <param name="processName"></param>
+        /// <param name="id"></param>
+        /// <param name="entireProcessTree"></param>
+        public void KillProcess(string processName)
+        {
+            foreach (var process in Process.GetProcessesByName(processName))
+            {
+                process.Kill(true);
+                return;
+            }
+            return;
+        }
+
+        /// <summary>
+        /// Check if process is running.
+        /// </summary>
+        /// <param name="processName"></param>
+        /// <returns></returns>
+        public bool IsProcess(string processName)
+        => Process.GetProcessesByName(processName).Length > 0;
+    }
+}
