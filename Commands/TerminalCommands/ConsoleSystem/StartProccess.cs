db2677839f3fa3c3fb71f81ff72731fab74ae810
x_coding <xcoding.dev@gmail.com>
2025-08-31 15:50:51 +0300

    wip: work on progreass

--- Commands/TerminalCommands/ConsoleSystem/StartProccess.cs
+++ Commands/TerminalCommands/ConsoleSystem/StartProccess.cs
@@ -17,6 +17,7 @@ Can be used with the following parameters:
     -h    : Displays this message.
     -u    : Can run process with different user.
     -we   : Disable wait for process to exit.
+    -wi   : Show new process window.
     -param: Start process with specified parameters.
          Example1: ./ -u <file_name>
          Example2: ./ -u <file_name> -param <file_paramters>
@@ -42,9 +43,15 @@ Both examples can be used with -we parameter.
                 string paramApp = string.Empty;
                 bool waitForExit = false;
 
-                if(args.Contains("-we"))
+                if(args.Contains("-wi"))
                 {
-                    args = args.Replace("-we".Trim(), string.Empty);
+                    args = args.Replace(" -wi".Trim(), string.Empty);
+                    waitForExit = true;
+                }
+
+                if (args.Contains("-we"))
+                {
+                    args = args.Replace(" -we".Trim(), string.Empty);
                     waitForExit = true;
                 }
 
@@ -118,7 +125,7 @@ Both examples can be used with -we parameter.
 
                 if (admin)
                 {
-                    Core.SystemTools.ProcessStart.ProcessExecute(inputCommand, arg, true, true, waitForExit);
+                    Core.SystemTools.ProcessStart.ProcessExecute(inputCommand, arg, true, true, waitForExit, bool isWindow);
                     return;
                 }
                 Core.SystemTools.ProcessStart.ProcessExecute(inputCommand, arg, true, false, waitForExit);
