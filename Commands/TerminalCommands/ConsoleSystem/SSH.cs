4f9fb0e73b0926cdc8360e23a02b00fa603d5969
x_coding <xcoding.dev@gmail.com>
2025-11-04 22:39:40 +0200

    created class for ssh command

---
+++ Commands/TerminalCommands/ConsoleSystem/SSH.cs
@@ -0,0 +1,47 @@
+﻿/*
+ 
+SSH Command wrapper for terminal. 
+
+ */
+
+using Core;
+using System;
+using System.Runtime.Versioning;
+using SystemCmd = Core.Commands.SystemCommands;
+
+namespace Commands.TerminalCommands.ConsoleSystem
+{
+    [SupportedOSPlatform("Windows")]
+    public class SSH : ITerminalCommand
+    {
+        public string Name => "ssh";
+        public void Execute(string arg)
+        {
+            try
+            {
+                GlobalVariables.isErrorCommand = false;
+
+                if (arg == Name && !GlobalVariables.isPipeCommand)
+                {
+                    FileSystem.SuccessWriteLine($"Use -h param for {Name} command usage!");
+                    return;
+                }
+                if (arg == $"{Name} -h")
+                {
+                    SystemCmd.SSHCmd(string.Empty);
+                    return;
+                }
+                if (GlobalVariables.isPipeCommand && GlobalVariables.pipeCmdCount == 0 || GlobalVariables.pipeCmdCount < GlobalVariables.pipeCmdCountTemp)
+                    arg = arg.Replace("ssh", GlobalVariables.pipeCmdOutput.Trim());
+                else
+                    arg = arg.Substring(3).Trim();
+                SystemCmd.SSHCmd(arg);
+            }
+            catch (Exception e)
+            {
+                FileSystem.ErrorWriteLine($"{e.Message}. Use -h for more information!");
+                GlobalVariables.isErrorCommand = true;
+            }
+        }
+    }
+}
