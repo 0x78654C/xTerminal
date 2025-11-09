e1729a241513b667d46a224d74675eacaa16418c
x_coding <xcoding.dev@gmail.com>
2025-11-10 01:08:03 +0200

    wip - fxp command for file exploer

---
+++ Commands/TerminalCommands/DirFiles/xPlorer.cs
@@ -0,0 +1,26 @@
+﻿using Core;
+using Core.DirFiles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commands.TerminalCommands.DirFiles
+{
+    public class xPlorer : ITerminalCommand
+    {
+        /*
+         * Simple hex viewer.
+         */
+
+        public string Name => "fxp";
+        public void Execute(string args)
+        {
+            var currentDir = File.ReadAllText(GlobalVariables.currentDirectory);
+            var fExplorer = new FileExplorer(currentDir);
+            fExplorer.Run();
+        }
+    }
+}
