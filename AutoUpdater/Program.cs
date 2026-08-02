14ffa1b0a828438569eaa7659ad8f4bf43430edf
x_coding <xcoding.dev@gmail.com>
2026-08-02 17:00:00 +0300

    addded kill xterminal and removed parts that is needed in shell.cs

--- AutoUpdater/Program.cs
+++ AutoUpdater/Program.cs
@@ -1,6 +1,7 @@
-﻿using System.Reflection;
-using System.Runtime.Versioning;
-using System.Windows.Forms;
+﻿using Core;
+using Core.Updater;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace AutoUpdater
 {
@@ -8,14 +9,51 @@ namespace AutoUpdater
     {
         static void Main(string[] args)
         {
-            var pathExecutable = Path.GetDirectoryName(Application.ExecutablePath);
-           // var xterminalDll = @$"{pathExecutable}\xTerminal.dll"; 
-           var xterminalDll = Path.Combine("C:\\Users\\MrX\\Projects\\xTerminal\\Shell\\bin\\x64\\Debug\\net10.0-windows7.0\\xTerminal.dll");
-            var verExe = File.Exists(xterminalDll) ? AssemblyName.GetAssemblyName(xterminalDll).Version.ToString() : "File does not exist!";
-            var arch = Environment.Is64BitOperatingSystem ? "x64" : "x86";  
-            var githubAPI = new GitHubAPI();
-            var releases = Task.Run(()=>githubAPI.ListReporsetories(verExe, arch));
-            Console.ReadKey();
+            try
+            {
+                KillXterm();
+                var xtermPath = "";
+
+                try
+                {
+                    xtermPath = args[0];
+                }
+                catch { }
+                var pathExecutable = Path.GetDirectoryName(Application.ExecutablePath);
+                //   var xterminalDll = @$"{xtermPath}\xTerminal.dll"; 
+                var xterminalDll = "C:\\Users\\mrx\\Projects\\xTerminal\\Release\\net10.0-windows7.0\\xTerminal.dll";
+                var verExe = File.Exists(xterminalDll) ? AssemblyName.GetAssemblyName(xterminalDll).Version.ToString() : "File does not exist!";
+                var arch = Environment.Is64BitOperatingSystem ? "x64" : "x86";
+                var githubAPI = new GitHubAPI();
+                Task.Run(() => githubAPI.CheckNewVersions(verExe, arch)).Wait();
+                githubAPI.DownloadUpdate();
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                FileSystem.ErrorWriteLine(ex.ToString());
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// Kill xTerminal.
+        /// </summary>
+        /// <returns></returns>
+        private static bool KillXterm()
+        {
+            var processes = Process.GetProcesses();
+
+            foreach (var process in processes)
+            {
+                if (process.ProcessName == "xTerminal")
+                {
+                    process.Kill();
+                    process.WaitForExit();
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
