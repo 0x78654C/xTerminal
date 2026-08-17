c2a3f5322e481f797f60fd61da0d01a109c7ca75
xeL <xcoding.dev@gmail.com>
2026-08-17 23:56:06 +0300

    added global variables to manage paths

---
+++ AutoUpdater/Utils/GlobalVariables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoUpdater.Utils
+{
+    public class GlobalVariables
+    {
+        public static readonly string accountName = Environment.UserName;
+        public static string rootPath = $"{Path.GetPathRoot(Environment.SystemDirectory)}Users\\{accountName}\\";
+        public static readonly string computerName = Environment.MachineName;
+        public static string terminalWorkDirectory = $"{Path.GetPathRoot(Environment.SystemDirectory)}Users\\{accountName}\\AppData\\Local\\xTerminal";
+        public static string unpackUpdate = $"{terminalWorkDirectory}\\Unpack\\";
+    }
+}
