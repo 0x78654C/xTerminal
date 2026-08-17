c4eb06fb1d9cd44bb9681b2f6c520a1d7d87ffcc
xeL <xcoding.dev@gmail.com>
2026-08-17 23:55:35 +0300

    added encryption class for adding in feature new stuff

---
+++ AutoUpdater/Utils/Encryption.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+
+namespace AutoUpdater.Utils
+{
+    public static class Encryption
+    {
+        /// <summary>
+        /// Get SHA256 hash
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static string GetSHA256(string file)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                using (var stream = File.OpenRead(file))
+                {
+                    var hash = sha256.ComputeHash(stream);
+                    var sourceSHA256 = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                    return sourceSHA256;
+                }
+            }
+        }
+    }
+}
