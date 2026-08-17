dca4c353f1a73791fe1bec89c78ac5d8d528c87f
xeL <xcoding.dev@gmail.com>
2026-08-17 23:56:18 +0300

    error write display

---
+++ AutoUpdater/Utils/UI.cs
@@ -0,0 +1,17 @@
+﻿namespace AutoUpdater
+{
+    public static class UI
+    {
+        /// <summary>
+        /// Write error output in color Red.
+        /// </summary>
+        /// <param name="text"></param>
+        public static void ErrorWriteLine(object data)
+        {
+            ConsoleColor currentForeground = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine($"Error: {data}");
+            Console.ForegroundColor = currentForeground;
+        }
+    }
+}
