07b700f73716d04b170a67bf0e7ba94798d3bebb
xeL <xcoding.dev@gmail.com>
2026-08-23 23:47:55 +0300

    added command xup for enable/disable auto updates on startup and chek for new updates

---
+++ Commands/TerminalCommands/ConsoleSystem/UpdaterAuto.cs
@@ -0,0 +1,58 @@
+﻿using Core;
+using Core.Updater;
+using System;
+using System.Runtime.Versioning;
+
+namespace Commands.TerminalCommands.ConsoleSystem
+{
+    [SupportedOSPlatform("Windows")]
+    public class UpdaterAuto : ITerminalCommand
+    {
+        public string Name => "xup";
+        private string _helpMessage = @"Usage of xup command:
+
+    xup -e : Enables checking for updates on startup.
+    xup -d : Disables checking for updates on startup.
+    xup -c : Checks for updates and displays the result.
+    xup -h : Displays this help message.
+";
+
+        public void Execute(string arg)
+        {
+            try
+            {
+                if (arg == $"{Name} -h")
+                {
+                    Console.WriteLine(_helpMessage);
+                    return;
+                }
+                arg = arg.Replace($"{Name} ", "").Trim();
+
+                // Enable
+                if (arg == "-e")
+                {
+                    RegistryManagement.regKey_WriteSubkey(GlobalVariables.regKeyName, GlobalVariables.regAutoUpdate, "True");
+                    FileSystem.SuccessWriteLine("Auto-update check on startup has been enabled.");
+                }
+
+                // Disable
+                if (arg == "-d")
+                {
+                    RegistryManagement.regKey_WriteSubkey(GlobalVariables.regKeyName, GlobalVariables.regAutoUpdate, "False");
+                    FileSystem.SuccessWriteLine("Auto-update check on startup has been disabled.");
+                }
+
+                if (arg == "-c")
+                {
+                    var gitApi = new GitHubAPI();
+                    gitApi.CheckUpdate();
+                }
+            }
+            catch (Exception e)
+            {
+                FileSystem.ErrorWriteLine(e.Message);
+                GlobalVariables.isErrorCommand = true;
+            }
+        }
+    }
+}
